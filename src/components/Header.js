15d98750f35d6b6ed5903383cf0b59c95647a31d
Teoman Soygul <tsoygul@gmail.com>
2017-12-08 23:56:15 +0100

    ui: reorder header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,23 +5,14 @@ import './Header.css';
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
-  }
-
-  componentDidMount = () => this.timerID = setInterval(() => this.setState({date: new Date()}), 1000);
-  componentWillUnmount = () => clearInterval(this.timerID);
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
+    this.state = {login: false};
   }
 
   render() {
     return (
       <header className="Header-header">
         <img src={logo} className="Header-logo" alt="logo" />
-        <h1 className="Header-title">It is {this.state.date.toLocaleTimeString()}.</h1>
+        <h1 className="Header-title">It is.</h1>
       </header>
     );
   }
