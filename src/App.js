d8a6409ce0f2bc3d3485c25aba8332b403bd5a9c
Teoman Soygul <tsoygul@gmail.com>
2017-12-13 16:13:30 +0100

    ui: remove material components until we need them

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
 import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <div className="App">
-          <Header />
+      <div className="App">
+        <Header />
 
-          <p className="App-body">
-            Body text.
-          </p>
-        </div>
-      </MuiThemeProvider>
+        <p className="App-body">
+          Body text.
+        </p>
+      </div>
     );
   }
 }
