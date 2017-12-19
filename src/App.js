a38b162478f794d17f88cbc25a827b97321818f9
Teoman Soygul <tsoygul@gmail.com>
2017-12-19 09:25:16 +0100

    ui: simplify hero

--- src/App.js
+++ src/App.js
@@ -27,17 +27,15 @@ class App extends Component {
         </div>
 
         <div className="hero is-medium is-light">
-          <div className="hero-body">
-            <div className="container has-text-centered">
-              <h1 className="title">Send and Receive Majorna</h1>
-              <h2 className="subtitle">Simple crypto currency with built-in banking services.</h2>
-              <a className="button is-medium is-info">Get Started</a>
-            </div>
+          <div className="hero-body has-text-centered">
+            <h1 className="title">Send and Receive Majorna</h1>
+            <h2 className="subtitle">Simple crypto currency with built-in banking services.</h2>
+            <a className="button is-medium is-info">Get Started</a>
           </div>
         </div>
 
         <div className="App-footer is-size-6">
-          &copy; 2018 Majorna Team &middot; Source code is MIT licensed &middot; <a className="button is-small is-info is-outlined" href="https://github.com/majorna" target="_blank">View on GitHub</a>
+          &copy; 2018 Majorna Team &middot; Source code is MIT licensed &middot; <a className="button is-small is-info is-outlined" href="https://github.com/majorna" target="_blank" rel="noopener noreferrer">View on GitHub</a>
         </div>
       </React.Fragment>
     );
