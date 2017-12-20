2ca9a7ac4bd15c453e5fbf6a1641e43c76caed7b
Teoman Soygul <tsoygul@gmail.com>
2017-12-20 11:12:51 +0100

    ui: hero: slight spacing between title and sub

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ export default class App extends Component {
 
         <div className="hero is-medium is-light">
           <div className="hero-body has-text-centered">
-            <h1 className="is-size-1">Send and Receive Majorna</h1>
-            <h2 className="subtitle">Simple crypto currency with built-in banking services.</h2>
+            <h1 className="title is-size-1">Send and Receive Majorna</h1>
+            <h2 className="subtitle" style={{marginTop: '0.5rem'}}>Simple crypto currency with built-in banking services.</h2>
             <a className="button is-medium is-info">Get Started</a>
           </div>
         </div>
 
         <div className="App-footer is-size-6">
-          &copy; 2018 Majorna Team &middot; Source code is MIT licensed &middot; <a className="button is-small is-info is-outlined" href="https://github.com/majorna" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+          &copy; 2018 Majorna Team &middot; Source code is MIT &middot; <a className="button is-small is-info is-outlined" href="https://github.com/majorna" target="_blank" rel="noopener noreferrer">View on GitHub</a>
         </div>
       </React.Fragment>
     );
