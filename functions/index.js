3d4245ba3cde0b22eb895b726dd76fea4abffb76
Teoman Soygul <tsoygul@gmail.com>
2017-12-25 03:18:34 +0100

    svr: create user document on first login

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 exports.ping = functions.https.onRequest((request, response) => response.send('pong'))
 
-// exports.createFirestoreUserDocuments = functions.auth.user().onCreate(event => {
-//   const user = event.data
-//   const email = user.email
-//   const displayName = user.displayName
-// })
-//
+exports.createFirestoreUserDocument = functions.auth.user().onCreate(event => {
+  const user = event.data
+  const id = user.uid
+  const email = user.email
+  const displayName = user.displayName
+
+  return admin.firestore().collection('users').doc(id).add({
+    email: email,
+    displayName: displayName,
+    created: new Date(),
+    balance: 500
+  })
+})
+
 // exports.createFirestoreUserDocuments = functions.auth.user().onDelete(event => {
 //   const user = event.data
 //   const email = user.email
