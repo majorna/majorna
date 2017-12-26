9363dd2901f8b783a48f94e27dd1f19843de4d17
Teoman Soygul <tsoygul@gmail.com>
2017-12-26 19:39:26 +0100

    ui: add reddit to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,5 +5,6 @@ export default () => (
     © 2018 Majorna Team
     · Source code is MIT
     · <a className="button is-small is-info is-outlined" href="https://github.com/majorna" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+    &nbsp;· <a className="button is-small is-info is-outlined" href="https://www.reddit.com/r/majorna/" target="_blank" rel="noopener noreferrer">Reddit</a>
   </div>
 );
