d3ee4dd4baff7e794dc78e64c372496c3409f21d
Teoman Soygul <tsoygul@gmail.com>
2017-12-27 15:38:24 +0100

    ui: add m-m sizing class

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default () => (
-  <div className="App-footer is-size-6">
+  <div className="m-m is-size-6">
     © 2018 Majorna Team
     · Source code is MIT
     · <a className="button is-small is-info is-outlined" href="https://github.com/majorna" target="_blank" rel="noopener noreferrer">View on GitHub</a>
