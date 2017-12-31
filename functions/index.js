42ecfaf3b4c43e277d72b20498ddd282aa87903d
Teoman Soygul <tsoygul@gmail.com>
2017-12-31 21:17:12 +0100

    svr: create initial transaction upon first auth

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,19 @@ const fbFunctions = require('firebase-functions');
 const fbAdmin = require('firebase-admin');
 fbAdmin.initializeApp(fbFunctions.config().firebase);
 
+function newId() {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let autoId = '';
+  for (let i = 0; i < 20; i++) {
+    autoId += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return autoId;
+}
+
 exports.ping = fbFunctions.https.onRequest((request, response) => response.send('pong'))
 
 exports.createFirestoreUserDocument = fbFunctions.auth.user().onCreate(event => {
+  const time = fbAdmin.firestore.FieldValue.serverTimestamp();
   const user = event.data
   const id = user.uid
   const email = user.email
@@ -13,8 +23,16 @@ exports.createFirestoreUserDocument = fbFunctions.auth.user().onCreate(event => 
   return fbAdmin.firestore().collection('users').doc(id).set({
     email: email,
     displayName: displayName,
-    created: fbAdmin.firestore.FieldValue.serverTimestamp(),
-    balance: 500
+    created: time,
+    balance: 500,
+    transactions: [
+      {
+        id: newId(),
+        sent: time,
+        from: 'majorna',
+        amount: 500
+      }
+    ]
   })
 })
 
