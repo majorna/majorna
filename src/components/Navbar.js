15f3d86860b7ac51d02e8e41f989a501de617159
Teoman Soygul <tsoygul@gmail.com>
2018-01-15 18:50:47 +0100

    ui: nav: replace fragment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,21 +25,21 @@ export default class extends Component {
           <div className={"navbar-burger" + (this.state.navOpen ? ' is-active' : '')} onClick={this.handleExpandClick}><span/><span/><span/></div>
         </div>
         <div className={"navbar-menu" + (this.state.navOpen ? ' is-active' : '')}>
-          <div className="navbar-end">
-            {!this.props.user ? (
+          {!this.props.user ? (
+            <div className="navbar-end">
               <Link className="navbar-item" to='/login' onClick={this.closeNav}>Log In | Sign Up</Link>
-            ) : (
-              <React.Fragment>
-                <Link className="navbar-item" to='/dashboard' onClick={this.closeNav}>Dashboard</Link>
-                <div className="navbar-item has-dropdown is-hoverable">
-                  <a className="navbar-link">{this.props.user.displayName}</a>
-                  <div className="navbar-dropdown is-boxed is-right">
-                    <Link className="navbar-item" to='/' onClick={this.logout}>Logout</Link>
-                  </div>
+            </div>
+          ) : (
+            <div className="navbar-end">
+              <Link className="navbar-item" to='/dashboard' onClick={this.closeNav}>Dashboard</Link>
+              <div className="navbar-item has-dropdown is-hoverable">
+                <a className="navbar-link">{this.props.user.displayName}</a>
+                <div className="navbar-dropdown is-boxed is-right">
+                  <Link className="navbar-item" to='/' onClick={this.logout}>Logout</Link>
                 </div>
-              </React.Fragment>
-            )}
-          </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     );
