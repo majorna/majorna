0786b204a36ca046711d7b328a2354074816c3bc
Teoman Soygul <tsoygul@gmail.com>
2018-01-17 14:18:01 +0100

    svr: add fb auth token template

--- server/fb-conf.js
+++ server/fb-conf.js
@@ -17,6 +17,28 @@ if (!isCloudFn) {
 
 const auth = admin.auth()
 
+const decodedTokenTemplate = {
+  iss: 'https://securetoken.google.com/majorna-fire',
+  name: 'Chuck Norris',
+  picture: 'https://lh3.googleusercontent.com/abc/def/photo.jpg',
+  aud: 'majorna-fire',
+  auth_time: 1516094974,
+  user_id: 'dsafasdgasfgsadsdafdsfa',
+  sub: '234sdfsgasdfsadf',
+  iat: 1516191924,
+  exp: 1516195524,
+  email: 'chuck.norris@gmail.com',
+  email_verified: true,
+  firebase: {
+    identities: {
+      'google.com': ['1232343453464654'],
+      email: ['chuck.norris@gmail.com']
+    },
+    sign_in_provider: 'google.com'
+  },
+  uid: 'dsafasdgasfgsadsdafdsfa'
+}
+
 exports.verifyTokenMiddleware = async function () {
   // token is in: headers = {Authorization: 'Bearer ' + token}
   const token = ''
