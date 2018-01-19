7b31505a5774dc56c822dc93562367939b402a4c
Teoman Soygul <tsoygul@gmail.com>
2018-01-19 11:17:20 +0100

    svr: user: create user after tx

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,37 +1,42 @@
+const FieldValue = require("firebase-admin").firestore.FieldValue
+
 /**
  * Create user doc and push first bonus transaction.
+ * Can be used as a firestore cloud function trigger.
  */
 exports.createUserDoc = async (firestore, user) => {
   const uid = user.uid
   const email = user.email
   const name = user.name || user.displayName // firebase auth token || firestore event
 
-  console.log(`created user: ${uid} - ${email} - ${displayName}`)
+  const time = FieldValue.serverTimestamp()
+  const initBalance = 500
+
+  // create the first transaction for the user
+  const txDoc = await firestore.collection('txs').add({
+    from: 'majorna',
+    to: uid,
+    sent: time,
+    amount: initBalance
+  })
 
   // create user doc
-  // todo: all db operations below should be combined into one transaction so this functions will be idempotent
-  const dbPromise = firestore.collection('users').doc(uid).set({
+  await firestore.collection('users').doc(uid).set({
     email: email,
     name: name,
-    created: firebase.firestore.FieldValue.serverTimestamp(),
-    balance: 500,
+    created: time,
+    balance: initBalance,
     transactions: [
       {
-        id: event.eventId,
-        sent: new Date(event.timestamp),
+        id: txDoc.id,
         from: 'majorna',
-        amount: 500
+        sent: txDoc.sent,
+        amount: initBalance
       }
     ]
   })
 
-  // append the tx to tx collection
-  // dbPromise.then(() => {
-  //   return firebase.firestore().collection('txs').doc(event.eventId).set({
-  //
-  //   })
-  // })
+  // update market cap
 
-  // update market cap (create necessary fields if they don't exist)
-  return dbPromise
+  console.log(`created user: ${uid} - ${email} - ${displayName}`)
 }
