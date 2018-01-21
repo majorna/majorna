b94637bc2897428545d9894dff8b1b1244e78aa9
Teoman Soygul <tsoygul@gmail.com>
2018-01-21 14:20:14 +0100

    svr: db: move addTx to top

--- server/data/db.js
+++ server/data/db.js
@@ -14,29 +14,36 @@ exports.updateMarketCap = amount => firestore.runTransaction(async t => {
   await t.update(metaRef, {cap: metaDoc.data().cap + amount})
 })
 
+exports.getTx = async id => {
+  const tx = await txsRef.doc(id).get()
+  return tx.exists ? tx.data() : null
+}
+
 exports.addTx = (from, to, sent, amount) => firestore.runTransaction(async t => {
   // verify sender's funds
   const senderDoc = await t.get(usersRef.doc(from))
-  if (!senderDoc.exists || senderDoc.data().balance < amount) {
+  const sender = senderDoc.data()
+  if (!senderDoc.exists || sender.balance < amount) {
     throw new Error('insufficient funds')
   }
 
   // check if receiver exists
   const receiverDoc = await t.get(usersRef.doc(from))
+  const receiver = receiverDoc.data()
   if (!receiverDoc.exists) {
     throw new Error('receiver does not exist')
   }
 
   // add tx to txs collection
-  await t.create(txsRef.doc(), {from, to, sent, amount})
+  const txRef = txsRef.doc()
+  await t.create(txRef, {from, to, sent, amount})
 
-  // update user docs with tx
+  // update user docs with tx and updated balances
+  sender.txs.unshift({id: txRef.id, to, sent, amount})
+  await t.update(senderDoc, {balance: sender.balance - amount, txs: sender.txs})
+  receiver.txs.unshift({id: txRef.id, from, sent, amount})
+  await t.update(receiverDoc, {balance: receiver.balance + amount, txs: receiver.txs})
 })
-
-exports.getTx = async id => {
-  const tx = await txsRef.doc(id).get()
-  return tx.exists ? tx.data() : null
-}
 
 /**
  * Create user doc and push first bonus transaction.
@@ -61,7 +68,7 @@ exports.createUserDoc = async user => {
     name: name,
     created: time,
     balance: initBalance,
-    transactions: [
+    txs: [
       {
         id: txRef.id,
         from: 'majorna',
