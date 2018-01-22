d207963d3d527b4b54b4f32396ab3aea2f557e60
Teoman Soygul <tsoygul@gmail.com>
2018-01-22 19:30:05 +0100

    svr: log fb init modes

--- server/config/config.js
+++ server/config/config.js
@@ -19,12 +19,15 @@ const fb = exports.firebase = {
 }
 
 if (app.isCloudFn) { // Google Cloud Functions
+  console.log('config: firebase: cloud functions mode')
   fb.config = require('firebase-functions').config().firebase
 } else if (fb.serviceKeyJsonPath) { // local or manual configuration
+  console.log('config: firebase: local/manual mode')
   const serviceJson = require(fb.serviceKeyJsonPath)
   fb.credentials = firebaseAdmin.credential.cert(serviceJson)
   fb.config = {credential: fb.credentials}
 } else { // Google Compute Engine
+  console.log('config: firebase: gce mode')
   // required GCE access scopes for default credentials to work: https://firebase.google.com/docs/admin/setup
   fb.credentials = firebaseAdmin.credential.applicationDefault()
   fb.config = {credential: fb.credentials}
