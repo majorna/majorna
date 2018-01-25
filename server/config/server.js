19e7e0f120116be0c9a569953cc3d2227c96aaba
Teoman Soygul <tsoygul@gmail.com>
2018-01-25 13:37:46 +0100

    svr: factor out server config

---
+++ server/config/server.js
@@ -0,0 +1,12 @@
+const db = require('./data/db')
+const config = require('./config/config')
+const koaConfig = require('./config/koa')
+
+module.exports = async () => {
+  await db.init()
+  const koaApp = koaConfig()
+  koaApp.listen(config.app.port)
+  if (!config.app.isTest) {
+    console.log('server listening on port ' + config.app.port)
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,14 +1,3 @@
-const db = require('./data/db')
-const config = require('./config/config')
-const koaConfig = require('./config/koa')
+const server = require('./config/server')
 
-module.exports = async () => {
-  await db.init()
-  const koaApp = koaConfig()
-  koaApp.listen(config.app.port)
-  if (!config.app.isTest) {
-    console.log('server listening on port ' + config.app.port)
-  }
-}
-
-module.exports()
+server()
