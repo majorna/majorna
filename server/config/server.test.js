403c04305d34f44cdd140f0cf8f9e6d3a9e76838
Teoman Soygul <tsoygul@gmail.com>
2018-01-31 18:43:52 +0100

    svr: add server test file

---
+++ server/config/server.test.js
@@ -0,0 +1,12 @@
+// const assert = require('assert')
+// const testData = require('../config/test').data
+
+suite('server-config', () => {
+  test('invalid token auth', async () => {
+    // todo: verify unauthorized request = 401 etc.
+    // todo: verify valid token auth
+    // todo: verify server crash/exception from route
+    // const res = await testData.users.u4Request.get('/users/init')
+    // assert(res.status === 204)
+  })
+})
