97b04cf264f3f91f75ab5dff09baad0537eb18f5
Teoman Soygul <tsoygul@gmail.com>
2018-01-31 18:30:35 +0100

    svr: allow firebase credentials as env string

--- server/config/config.js
+++ server/config/config.js
@@ -16,9 +16,10 @@ const app = {
 
 // firebase config
 const fb = {
-  serviceKeyJsonPath: process.env.FIREBASE_JSON_PATH,
-  testServiceKeyJsonPath: process.env.FIREBASE_TEST_JSON_PATH, // test (admin sdk) only
-  testClientSdkKeyJsonPath: process.env.FIREBASE_CLIENT_TEST_JSON_PATH, // test (client sdk) only
+  serverKeyJson: process.env.MAJORNA_FIREBASE_JSON,
+  serviceKeyJsonPath: process.env.MAJORNA_FIREBASE_JSON_PATH,
+  testServiceKeyJsonPath: process.env.MAJORNA_FIREBASE_TEST_JSON_PATH, // test (admin sdk) only
+  testClientSdkKeyJsonPath: process.env.MAJORNA_FIREBASE_CLIENT_TEST_JSON_PATH, // test (client sdk) only
   credentials: null,
   config: {
     credential: null
@@ -33,9 +34,9 @@ if (app.isTest) { // test config with config file
 } else if (app.isCloudFn) { // Google Cloud Functions
   console.log('config: firebase: cloud functions mode')
   fb.config = require('firebase-functions').config().firebase
-} else if (fb.serviceKeyJsonPath) { // local or manual configuration
+} else if (fb.serviceKeyJsonPath || fb.serverKeyJson) { // local or manual configuration
   console.log('config: firebase: local/manual mode')
-  const serviceJson = require(fb.serviceKeyJsonPath)
+  const serviceJson = (fb.serviceKeyJsonPath && require(fb.serviceKeyJsonPath)) || JSON.parse(fb.serverKeyJson)
   fb.credentials = firebaseAdmin.credential.cert(serviceJson)
   fb.config = {credential: fb.credentials}
 } else { // Google Compute Engine
