6415ddff79be7e3853d6e40df00331ffa6e0a92c
Teoman Soygul <tsoygul@gmail.com>
2018-02-01 22:26:42 +0100

    svr: test: more firebase tests

--- server/config/firebase.test.js
+++ server/config/firebase.test.js
@@ -4,13 +4,16 @@ const firebaseConfig = require('./firebase')
 
 suite('firebase-config', () => {
   test('firestore access rules', () => {
-    // todo: people can only read their user doc and cannot write to it.
-    // todo: people can't read others' transactions, or write to it.
+    // todo: people can only read their user doc and cannot write to it
+    // todo: people can't read others' transactions
+    // todo: none of the collections are writeable
   })
 
   test('verifyIdToken', async () => {
+    const u1 = testData.users.u1Auth
     const decodedToken = await firebaseConfig.verifyIdToken(testData.users.u1Token)
-    assert(decodedToken.uid)
-    // todo: verify user data
+    assert(decodedToken.uid === u1.uid)
+    assert(decodedToken.email === u1.email)
+    assert(decodedToken.name === u1.displayName)
   })
 })
