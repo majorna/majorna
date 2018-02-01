ba50dda68046abe5969dfd6867daf20d0f140060
Teoman Soygul <tsoygul@gmail.com>
2018-02-01 20:45:09 +0100

    svr: move access rule test todos

--- server/config/firebase.test.js
+++ server/config/firebase.test.js
@@ -3,6 +3,11 @@ const testData = require('./test').data
 const firebaseConfig = require('./firebase')
 
 suite('firebase-config', () => {
+  test('firestore access rules', () => {
+    // todo: people can only read their user doc and cannot write to it.
+    // todo: people can't read others' transactions, or write to it.
+  })
+
   test('verifyIdToken', async () => {
     const decodedToken = await firebaseConfig.verifyIdToken(testData.users.u1Token)
     assert(decodedToken.uid)
