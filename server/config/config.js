689204ec96b1884167bcba19add839a09264b740
Teoman Soygul <tsoygul@gmail.com>
2018-02-05 23:48:03 +0100

    svr: use port 3002 for testing

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ const app = {
   isTest: env === 'test',
   isCloudFn: false, // if "require('firebase-functions').config().firebase" is defined
 
-  port: process.env.PORT || 3001
+  port: process.env.PORT || env !== 'test' ? 3001 : 3002
 }
 
 // firebase config
