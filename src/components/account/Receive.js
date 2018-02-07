099a970a431af74bca70f37b16f7fd49c24d376c
Teoman Soygul <tsoygul@gmail.com>
2018-02-07 17:23:31 +0100

    ui: receive: copy to clipboard button

--- src/components/account/Receive.js
+++ src/components/account/Receive.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { toClipboard } from '../../data/utils'
 
 export default props => (
-  <div className="mj-box flex-column">
-    <img width="72" src={props.acctQr} alt={props.user.uid}/>
+  <div className="mj-box flex-column center-all">
+    <div className="is-size-5">Account Address</div>
 
-    <div className="flex-row center-h m-t-l">
-      <button className="button" onClick={props.history.goBack}>Close</button>
+    <img width="248" src={props.acctQr} alt={props.user.uid}/>
+
+    <div className="flex-row m-t-l">
+      <strong>{props.user.uid}</strong>
+      <button className="button is-small is-info is-outlined m-l-s" onClick={() => toClipboard(props.user.uid)}>Copy</button>
     </div>
+
+    <button className="button m-t-l" onClick={props.history.goBack}>Close</button>
   </div>
 )
--- src/data/utils.js
+++ src/data/utils.js
@@ -20,3 +20,16 @@ export function getChartData(meta) {
   }
   return data
 }
+
+/**
+ * Copy given text to clipboard.
+ * Creates a temporary input element on page, assigns the text to it, selects the text, runs copy command, removes the element.
+ */
+export function toClipboard(text) {
+  const input = document.createElement('input')
+  input.value = text
+  document.body.appendChild(input)
+  input.select()
+  document.execCommand('copy')
+  document.body.removeChild(input)
+}
