48b4c11a24f74f44b5a735dea70701aa9436be16
Teoman Soygul <tsoygul@gmail.com>
2018-02-09 11:15:28 +0100

    svr: config: add github config

--- server/config/github.js
+++ server/config/github.js
@@ -1,7 +1,21 @@
 /**
  * GitHub Integration:
  * - API v3 ref: https://developer.github.com/v3/repos/contents/
+ * - Node.js SDK: https://github.com/octokit/rest.js
+ * - Node.js SDK ref: https://octokit.github.io/rest.js/#api-Repos-getContent
  */
+const octokit = require('@octokit/rest')()
+const config = require('./config')
+
+// token auth (https://github.com/settings/tokens)
+octokit.authenticate({
+  type: 'token',
+  token: config.github.token
+})
+
+// github api params
+const owner = config.github.owner
+const repo = config.github.repo
 
 /**
  * Creates a file with given data if it does not exist.
@@ -9,8 +23,8 @@
  * @param path - Path of the file in git repo.
  * @param data - Data to be appended at the end of the file.
  */
-exports.upsertFile = (path, data) => {
-
+exports.upsertFile = async (path, data) => {
+  const res = await octokit.repos.getContent({owner, repo, path})
 }
 
 /**
