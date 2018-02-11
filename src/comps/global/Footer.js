64257f1b47dde86e085ab1433a13de004902b633
Teoman Soygul <tsoygul@gmail.com>
2018-02-11 10:09:10 +0100

    ui: simplify footer

--- src/comps/global/Footer.js
+++ src/comps/global/Footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default () => (
   <div className="m-m is-size-6">
     © 2018 Majorna Team
-    · Source code is MIT
-    · <a className="button is-small is-info is-outlined" href="https://github.com/majorna" target="_blank" rel="noopener noreferrer">View on GitHub</a>
-    &nbsp;· <a className="button is-small is-info is-outlined" href="https://www.reddit.com/r/majorna/" target="_blank" rel="noopener noreferrer">Reddit</a>
+    · Open Source (MIT)
+    · <a className="button is-small is-outlined" href="https://github.com/majorna" target="_blank" rel="noopener noreferrer">View on GitHub</a>
+    &nbsp;· <a className="button is-small is-outlined" href="https://www.reddit.com/r/majorna/" target="_blank" rel="noopener noreferrer">Reddit</a>
   </div>
 )
