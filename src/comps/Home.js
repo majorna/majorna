20ed262d3d616e2433b042d93f15c61e25cf4a3c
Teoman Soygul <tsoygul@gmail.com>
2018-02-12 09:53:58 +0100

    ui: add first media item

--- src/comps/Home.js
+++ src/comps/Home.js
@@ -2,14 +2,28 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default () => (
-  <div className="hero is-medium is-light">
-    <div className="hero-body has-text-centered">
-      <h1 className="title is-size-1" style={{marginBottom: '3rem'}}>Send and Receive Majorna</h1>
-      <h2 className="subtitle" style={{marginBottom: '3rem'}}>Simple cryptocurrency, in your browser.</h2>
-      <div className="field is-grouped is-grouped-centered">
-        <p className="control is-hidden-mobile"><input readOnly className="input is-medium" type="text" placeholder="Starter Balance: 500mj" /></p>
-        <p className="control"><Link className="button is-medium is-info" to='/login'>Get Started</Link></p>
+  <React.Fragment>
+    <div className="hero is-medium is-light">
+      <div className="hero-body has-text-centered">
+        <h1 className="title is-size-1" style={{marginBottom: '3rem'}}>Send and Receive Majorna</h1>
+        <h2 className="subtitle" style={{marginBottom: '3rem'}}>Simple cryptocurrency, in your browser.</h2>
+        <div className="field is-grouped is-grouped-centered">
+          <p className="control is-hidden-mobile"><input readOnly className="input is-medium" type="text" placeholder="Starter Balance: 500mj" /></p>
+          <p className="control"><Link className="button is-medium is-info" to='/login'>Get Started</Link></p>
+        </div>
       </div>
     </div>
-  </div>
+
+    <div className="mj-box">Some Stats</div>
+
+    <div className="mj-box align-start">
+      <img src="http://via.placeholder.com/128x128" alt=""/>
+      <div className="flex-column flex-grow-1 m-l-m">
+        <div className="is-size-5">Media Title</div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        In erat mauris, faucibus quis pharetra sit amet, pretium ac libero.
+        Etiam vehicula eleifend bibendum.
+      </div>
+    </div>
+  </React.Fragment>
 )
