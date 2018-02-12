db4c83064f234c1dc9b37edf478c3547e9f57684
Teoman Soygul <tsoygul@gmail.com>
2018-02-12 16:21:59 +0100

    ui: home: link to reddit and public ledger

--- src/comps/Home.js
+++ src/comps/Home.js
@@ -24,7 +24,7 @@ export default () => (
       <div className="flex-column flex-grow-1 m-l-m">
         <div className="is-size-5">Simple Cryptocurrency</div>
         Easy to use cryptocurrency in your browser.
-        Login with your Google, Facebook, or Twitter account to start sending and receiving Majorna.
+        Log in with your Google, Facebook, or Twitter account to start sending and receiving Majorna.
         <Link to='/login'>+mj500 balance for all new accounts in 2018.</Link>
       </div>
     </div>
@@ -32,30 +32,28 @@ export default () => (
     <div className="mj-box align-start">
       <img className="flex-order-1 m-l-m" src="http://via.placeholder.com/128x128" alt=""/>
       <div className="flex-column flex-grow-1">
-        <div className="is-size-5">Media Title</div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        In erat mauris, faucibus quis pharetra sit amet, pretium ac libero.
-        Etiam vehicula eleifend bibendum.
+        <div className="is-size-5">Decentralized and Scalable</div>
+        Trustless and decentralized with simple in-browser miner.
+        Horizontal scalability through delegated trust.
       </div>
     </div>
 
     <div className="mj-box align-start">
       <img src="http://via.placeholder.com/128x128" alt=""/>
       <div className="flex-column flex-grow-1 m-l-m">
-        <div className="is-size-5">Media Title</div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        In erat mauris, faucibus quis pharetra sit amet, pretium ac libero.
-        Etiam vehicula eleifend bibendum.
+        <div className="is-size-5">Open Source</div>
+        <span>Easy to understand source code, licensed under MIT, hosted on <a href="https://github.com/majorna" target="_blank" rel="noopener noreferrer">GitHub</a>.</span>
+        <span>Community discussions hosted on <a href="https://www.reddit.com/r/majorna/" target="_blank" rel="noopener noreferrer">Reddit</a>.</span>
       </div>
     </div>
 
     <div className="mj-box align-start">
       <img className="flex-order-1 m-l-m" src="http://via.placeholder.com/128x128" alt=""/>
       <div className="flex-column flex-grow-1">
-        <div className="is-size-5">Media Title</div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        In erat mauris, faucibus quis pharetra sit amet, pretium ac libero.
-        Etiam vehicula eleifend bibendum.
+        <div className="is-size-5">Open Ledger</div>
+        <span>Public transaction logs are hosted at <a href="https://github.com/majorna/blockchain" target="_blank" rel="noopener noreferrer">GitHub MJ-Blockchain</a> repo.</span>
+        Sender and receiver identities are kept secret.
+        Anonymous transactions with batch-send is a work in progress.
       </div>
     </div>
   </React.Fragment>
