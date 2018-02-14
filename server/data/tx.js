c0183c64f3cb00edd167639a45dd180747870225
Teoman Soygul <tsoygul@gmail.com>
2018-02-14 14:40:00 +0100

    svr: add tx handler

---
+++ server/data/tx.js
@@ -0,0 +1,22 @@
+const db = require('./db')
+const github = require('./github')
+const crypto = require('./crypto')
+
+/**
+ * Strips majorna:/mj: prefixes from a string, if any.
+ */
+exports.stripPrefix = str => str.startsWith('mj:') ? str.substring(3) : str.startsWith('majorna:') ? str.substring(8) : str
+
+/**
+ * Inserts a tx in database and then github.
+ * If database step fails, transaction fails.
+ * todo: If github step fails, it is retried until success.
+ */
+exports.makeTx = async (from, to, amount) => {
+  from = exports.stripPrefix(from)
+  to = exports.stripPrefix(to)
+
+  const txData = await db.makeTx(from, to, amount)
+  const signedTx = crypto.signTx(txData)
+  await github.insertTxInBlock(signedTx)
+}
