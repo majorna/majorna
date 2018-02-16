22f1e8a736af4f06e5f9f6403b29e4de2163957f
Teoman Soygul <tsoygul@gmail.com>
2018-02-16 13:53:33 +0100

    svr: github: simplify todos

--- server/data/github.test.js
+++ server/data/github.test.js
@@ -6,7 +6,7 @@ const utils = require('./utils')
 const testFilePath = 'testfile'
 const readmePath = 'README.md'
 
-// todo: git reset --hard head~xxx after tests / or just repo.deleteFile/updateFile/deleteDirectory in 'suiteSetup in test.js'
+// todo: cleanup repo to 1st commit in 'suiteSetup in test.js'
 // todo: do 50 upserts at the same time and see if auto merge conflict resolution works (test 2: create file and update at the same time)
 // make sure that no data is lost (overwritten by old ones) and file has 50 lines total
 
@@ -25,5 +25,6 @@ suite('github', () => {
 
   test('insertTxInBlock', async () => {
     await github.insertTxInBlock(testData.txs[utils.getRandomInt(testData.txs.length)])
+    // todo: assert file content
   })
 })
