2451045067bc974438007bd45944870928e42cd9
Teoman Soygul <tsoygul@gmail.com>
2018-02-26 14:27:43 +0100

    ui: move miner to ui project

---
+++ src/comps/account/Miner.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default props => (
+  <div>
+    <span>Wow</span>
+  </div>
+)
+
+// exports.receiveTxs = () => {
+//   // no duplicates
+//   // no balance below 0
+//   // valid signatures
+// }
+//
+// exports.receiveBlock = () => {
+//   // validate each tx signature unless block is signed by a trusted key
+// }
