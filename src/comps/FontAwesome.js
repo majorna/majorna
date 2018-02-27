3f90f3b218bfc39fbc983c076021073f8f64a376
Teoman Soygul <tsoygul@gmail.com>
2018-02-27 11:32:58 +0100

    ui: mine: tidyup miner fields

--- src/comps/FontAwesome.js
+++ src/comps/FontAwesome.js
@@ -5,11 +5,11 @@ import fasGlobe from '@fortawesome/fontawesome-free-solid/faGlobe'
 import fabGithub from '@fortawesome/fontawesome-free-brands/faGithub'
 import fasPaperPlane from '@fortawesome/fontawesome-free-solid/faPaperPlane'
 import fasQrcode from '@fortawesome/fontawesome-free-solid/faQrcode'
-import farPlusSquare from '@fortawesome/fontawesome-free-regular/faPlusSquare'
+import fasTh from '@fortawesome/fontawesome-free-solid/faTh'
 
 fontawesome.library.add(fasCreditCard)
 fontawesome.library.add(fasGlobe)
 fontawesome.library.add(fabGithub)
 fontawesome.library.add(fasPaperPlane)
 fontawesome.library.add(fasQrcode)
-fontawesome.library.add(farPlusSquare)
+fontawesome.library.add(fasTh)
--- src/comps/account/Mine.js
+++ src/comps/account/Mine.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react'
+import { fm } from '../../data/utils'
 
 export default class extends Component {
   state = {
-    paymentLimit: 10,
-    earning: 0
+    progress: 0,
+    reward: 10, // todo: read this from mj/meta
+    minedBlocks: 0
   }
 
   componentDidMount() {
     // get txs and start mining
   }
 
-  handlePause = () => {}
   handleStop = () => this.props.history.goBack()
 
   render() {
     return (
-      <React.Fragment>
-        <div className="mj-box flex-column">
-          <div className="is-size-5 has-text-centered">Mining mj</div>
+      <div className="mj-box flex-column">
+        <div className="is-size-5 has-text-centered">Mining mj</div>
 
-          <strong>Progress</strong>
-          <progress className="progress is-large is-info" value="15" max="100">15.69%</progress>
+        <div><strong>Progress:</strong> {this.state.progress}%</div>
+        <progress className="progress is-large is-info" value={this.state.progress} max="100"/>
+
+        <div><strong>Earnings:</strong> mj{fm(this.state.reward * this.state.minedBlocks)}</div>
+        <div><strong>Mined Blocks:</strong> {this.state.minedBlocks}</div>
+        <div><strong>Reward per Block:</strong> mj{fm(this.state.reward)}</div>
+
+        <div className="flex-row center-h m-t-l">
+          <button className="button" onClick={this.handleStop}>Stop</button>
         </div>
-      </React.Fragment>
+      </div>
     )
   }
 }
