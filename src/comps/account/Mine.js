fb987bb6f506c074d8b7da93c79d6ea104f8618e
Teoman Soygul <tsoygul@gmail.com>
2018-02-28 16:10:03 +0100

    ui: mine: show mining timer

--- src/comps/account/Mine.js
+++ src/comps/account/Mine.js
@@ -6,22 +6,29 @@ export default class extends Component {
   state = {
     reward: 10, // todo: read this from mj/meta
     minedBlocks: 0,
-    hashRate: 0
+    hashRate: 0,
+    time: null
   }
 
   async componentDidMount() {
-    // call server and get last block header to start mining that block
+    // call server and get last block header
     const res = await server.blocks.mine()
     const miningParams = await res.json()
+
+    // start mining that block
+    const start = new Date().getTime()
     const alg = 'SHA-256'
-    const enc = new TextEncoder('utf-8')
-    const strBuffer = enc.encode(miningParams.str)
     let nonce = 0
     let lastNonce = 0
+    const enc = new TextEncoder('utf-8')
+    const strBuffer = enc.encode(miningParams.str)
     let nonceBuffer, fullStrArr, hashBuffer, hashArray, base64String
 
     this.interval = setInterval(() => {
-      this.setState({hashRate: nonce - lastNonce})
+      this.setState({
+        hashRate: nonce - lastNonce,
+        time: new Date(new Date().getTime() - start)
+      })
       lastNonce = nonce
     }, 1000)
 
@@ -30,15 +37,14 @@ export default class extends Component {
       nonce++
       nonceBuffer = enc.encode(nonce.toString())
       fullStrArr = new Uint8Array(nonceBuffer.length + strBuffer.length)
-      fullStrArr.set(nonceBuffer);
+      fullStrArr.set(nonceBuffer)
       fullStrArr.set(strBuffer, nonceBuffer.length)
       // todo: since data to be hashed is so small, async/await cycle takes a lot longer than actual hashing
       // node-forge is about 10x faster here (but needs breaks in the loop with setImmediate not to block the event loop forever)
-      // alternatively we can increase the input text size to make async call overhead negligible / or just sha3
+      // alternatively we can increase the input text size to make async call overhead negligible / or just sha3 or PoS variant
       hashBuffer = await crypto.subtle.digest(alg, fullStrArr.buffer)
       hashArray = new Uint8Array(hashBuffer)
       if (hashArray[0] === 0 && hashArray[1] === 0 && hashArray[2] === 0) {
-        console.log(hashArray)
         base64String = btoa(String.fromCharCode(...hashArray))
         console.log(`mined block with difficulty: ${miningParams.difficulty}, nonce: ${nonce}, hash: ${base64String}`)
         break
@@ -58,8 +64,9 @@ export default class extends Component {
         <div className="is-size-5 has-text-centered">Mining mj</div>
         <div className="flex-row center-all spinner m-t-l"/>
 
-        <div><strong>Progress:</strong> {this.state.hashRate}H/s</div>
-        <div><strong>Earnings:</strong> mj{fm(this.state.reward * this.state.minedBlocks)}</div>
+        <div><strong>Time:</strong> {this.state.time ? Math.round(this.state.time.getTime() / 1000) : 0}s</div>
+        <div><strong>Rate:</strong> {this.state.hashRate} Hash/s</div>
+        <div><strong>Reward:</strong> mj{fm(this.state.reward * this.state.minedBlocks)}</div>
         <div><strong>Mined Blocks:</strong> {this.state.minedBlocks}</div>
         <div><strong>Reward per Block:</strong> mj{fm(this.state.reward)}</div>
 
