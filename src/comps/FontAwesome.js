cf5708b223adfab28397067f46bfba5e84dd0eb8
Teoman Soygul <tsoygul@gmail.com>
2018-03-01 23:37:08 +0100

    ui: send: use icons

--- src/comps/FontAwesome.js
+++ src/comps/FontAwesome.js
@@ -6,6 +6,7 @@ import fabGithub from '@fortawesome/fontawesome-free-brands/faGithub'
 import fasPaperPlane from '@fortawesome/fontawesome-free-solid/faPaperPlane'
 import fasQrcode from '@fortawesome/fontawesome-free-solid/faQrcode'
 import fasTh from '@fortawesome/fontawesome-free-solid/faTh'
+import fasCheck from '@fortawesome/fontawesome-free-solid/faCheck'
 
 fontawesome.library.add(fasCreditCard)
 fontawesome.library.add(fasGlobe)
@@ -13,3 +14,4 @@ fontawesome.library.add(fabGithub)
 fontawesome.library.add(fasPaperPlane)
 fontawesome.library.add(fasQrcode)
 fontawesome.library.add(fasTh)
+fontawesome.library.add(fasCheck)
