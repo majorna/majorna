e151d0aa1d478d1de18ede85399f7544eab780a7
Teoman Soygul <tsoygul@gmail.com>
2018-03-01 11:56:28 +0100

    ui: miner: move core miner code to node.js

--- src/comps/account/Mine.js
+++ src/comps/account/Mine.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { fm } from '../../data/utils'
 import server from '../../data/server'
+import { mineBlock } from '../../data/node'
 
 export default class extends Component {
   state = {
@@ -10,79 +11,33 @@ export default class extends Component {
     time: null
   }
 
-  async componentDidMount() {
+  componentDidMount = async () => {
     // call server and get last block header
     const res = await server.blocks.mine()
     const miningParams = await res.json()
 
     // start mining that block
-    const start = new Date().getTime()
-    const alg = 'SHA-256'
-    let nonce = 0
-    let lastNonce = 0
-    let i, found
-    const enc = new TextEncoder('utf-8')
-    const strBuffer = enc.encode(miningParams.str)
-    const difficulty = miningParams.difficulty
-    let nonceBuffer, fullStrArr, hashBuffer, hashArray, base64String
-
-    this.interval = setInterval(() => {
-      this.setState({
-        hashRate: nonce - lastNonce,
-        time: new Date(new Date().getTime() - start)
-      })
-      lastNonce = nonce
-    }, 1000)
-
-    console.log(`starting mining loop with difficulty ${miningParams.difficulty}`)
-    while (this.interval) {
-      nonce++
-      nonceBuffer = enc.encode(nonce.toString())
-      fullStrArr = new Uint8Array(nonceBuffer.length + strBuffer.length)
-      fullStrArr.set(nonceBuffer)
-      fullStrArr.set(strBuffer, nonceBuffer.length)
-      // todo: since data to be hashed is so small, async/await cycle takes a lot longer than actual hashing
-      // node-forge is about 10x faster here (but needs breaks in the loop with setImmediate not to block the event loop forever)
-      // alternatively we can increase the input text size to make async call overhead negligible / or just sha3 or PoS variant
-      hashBuffer = await crypto.subtle.digest(alg, fullStrArr.buffer)
-      hashArray = new Uint8Array(hashBuffer)
-      i = 0
-      found = true
-      for (;i < difficulty; i++) {
-        if (hashArray[i] !== 0) found = false
-      }
-      if (found) {
-        base64String = btoa(String.fromCharCode(...hashArray))
-        console.log(`mined block with difficulty: ${difficulty}, nonce: ${nonce}, hash: ${base64String}`)
-        break
-      }
-    }
+    this.interval = await mineBlock(miningParams.str, miningParams.difficulty, s => this.setState(s))
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval)
-    this.interval = null
-  }
+  componentWillUnmount = () => clearInterval(this.interval)
 
   handleStop = () => this.props.history.goBack()
 
-  render() {
-    return (
-      <div className="mj-box flex-column">
-        <div className="is-size-5 has-text-centered">Mining mj</div>
-        <div className="flex-row center-all spinner m-t-l"/>
+  render = () =>
+    <div className="mj-box flex-column">
+      <div className="is-size-5 has-text-centered">Mining mj</div>
+      <div className="flex-row center-all spinner m-t-l"/>
 
-        <div><strong>Time:</strong> {this.state.time ? Math.round(this.state.time.getTime() / 1000) : 0}s</div>
-        <div><strong>Rate:</strong> {this.state.hashRate} Hash/s</div>
-        <div><strong>Reward:</strong> mj{fm(this.state.reward * this.state.minedBlocks)}</div>
-        <div><strong>Mined Blocks:</strong> {this.state.minedBlocks}</div>
-        <div><strong>Reward per Block:</strong> mj{fm(this.state.reward)}</div>
+      <div><strong>Time:</strong> {this.state.time ? Math.round(this.state.time.getTime() / 1000) : 0}s</div>
+      <div><strong>Rate:</strong> {this.state.hashRate} Hash/s</div>
+      <div><strong>Reward:</strong> mj{fm(this.state.reward * this.state.minedBlocks)}</div>
+      <div><strong>Mined Blocks:</strong> {this.state.minedBlocks}</div>
+      <div><strong>Reward per Block:</strong> mj{fm(this.state.reward)}</div>
 
-        <div className="flex-row center-h m-t-l">
-          {/*<button className="button" onClick={this.handleBackground}>Background</button>*/}
-          <button className="button" onClick={this.handleStop}>Stop</button>
-        </div>
+      <div className="flex-row center-h m-t-l">
+        {/*<button className="button" onClick={this.handleBackground}>Background</button>*/}
+        <button className="button" onClick={this.handleStop}>Stop</button>
       </div>
-    )
-  }
+    </div>
 }
--- src/data/node.js
+++ src/data/node.js
@@ -1,32 +1,58 @@
-exports.receiveTxs = () => {
+export const receiveTxs = () => {
   // no duplicates
   // no balance below 0
   // valid signatures
 }
 
-exports.receiveBlock = () => {
+export const receiveBlock = () => {
   // validate each tx signature unless block is signed by a trusted key
 }
 
-exports.initSimplePeer = () => {}
+export const initSimplePeer = () => {}
 
-exports.mineBlock = async () => {
-  // get txs for the last 10 minute period and start mining
-  const now = new Date() // get time snapshot to prevent drift
+export const mineBlock = async (headerStr, difficulty, progressCb) => {
+  const alg = 'SHA-256'
+  const start = new Date().getTime()
+  let nonce = 0
+  let lastNonce = 0
+  const enc = new TextEncoder('utf-8')
+  const headerStrBuffer = enc.encode(headerStr)
+  let nonceBuffer, fullStrArr, hashBuffer, hashArray, base64String
 
-  const start = new Date(now.getTime())
-  start.setMilliseconds(0)
-  start.setSeconds(0)
-  start.setMinutes(now.getMinutes() - now.getMinutes() % 10)
+  const interval = setInterval(() => {
+    progressCb({
+      hashRate: nonce - lastNonce,
+      time: new Date(new Date().getTime() - start)
+    })
+    lastNonce = nonce
+  }, 1000)
 
-  const end = new Date(now.getTime())
-  end.setMilliseconds(0)
-  end.setSeconds(0)
-  end.setMinutes(now.getMinutes() - 10 - now.getMinutes() % 10)
+  let i, found
+  console.log(`starting mining loop with difficulty ${difficulty}`)
+  while (interval) {
+    nonce++
+    nonceBuffer = enc.encode(nonce.toString())
+    fullStrArr = new Uint8Array(nonceBuffer.length + headerStrBuffer.length)
+    fullStrArr.set(nonceBuffer)
+    fullStrArr.set(headerStrBuffer, nonceBuffer.length)
+    // todo: since data to be hashed is so small, async/await cycle takes a lot longer than actual hashing
+    // (note: since it is async, we can massively parallelize this)
+    // node-forge is about 10x faster here (but needs breaks in the loop with setImmediate not to block the event loop forever)
+    // alternatively we can increase the input text size to make async call overhead negligible / or just sha3 or PoS variant
+    hashBuffer = await crypto.subtle.digest(alg, fullStrArr.buffer)
+    hashArray = new Uint8Array(hashBuffer)
+    i = 0
+    found = true
+    for (;i < difficulty; i++) {
+      if (hashArray[i] !== 0) found = false
+    }
+    if (found) {
+      base64String = btoa(String.fromCharCode(...hashArray))
+      console.log(`mined block with difficulty: ${difficulty}, nonce: ${nonce}, hash: ${base64String}`)
+      clearInterval(interval)
+      break
+    }
+  }
 
-  console.log(`starting to mine block for txs between [${start}] and [${end}]`)
-  const txsSnap = await this.props.db.collection('txs').where('time', '>=', start).where('time', '<', end).get()
-  const txs = txsSnap.docs.map(doc => doc.data())
-
-  console.log(txs)
+  return interval
 }
