ca9f2ed56f7c35592fb923f76e08409da77fe55b
Teoman Soygul <tsoygul@gmail.com>
2018-03-19 11:44:11 +0100

    svr: tx: add docs

--- server/blockchain/tx.js
+++ server/blockchain/tx.js
@@ -1,5 +1,8 @@
 const crypto = require('./crypto')
 
+/**
+ * Transaction schema. All fields are mandatory.
+ */
 exports.txSchema = {
   sig: '', // signature of the tx, signed by the sender (or majorna on behalf of sender)
   id: '', // ID of the transaction
@@ -15,9 +18,16 @@ exports.txSchema = {
   amount: 0 // amount being sent
 }
 
+/**
+ * Concatenates the the given tx into a regular string, fit for hashing.
+ *  * todo: include sig ??
+ */
 exports.getStr = tx =>
   '' + tx.id + tx.from.id + tx.from.balance + tx.to.id + tx.to.balance + tx.time.getTime() + tx.amount
 
+/**
+ * Creates a valid tx object out of a given object. Extra fields will not be used.
+ */
 exports.getObj = tx => ({
   sig: tx.sig,
   id: tx.id,
@@ -27,13 +37,23 @@ exports.getObj = tx => ({
   amount: tx.amount
 })
 
+/**
+ * Signs a tx with majorna certificate.
+ */
 exports.sign = tx => {
   const sigTx = exports.getObj(tx)
   sigTx.sig = crypto.signText(exports.getStr(sigTx))
   return sigTx
 }
 
+/**
+ * Verifies a given tx's signature.
+ */
 exports.verifySig = tx => crypto.verifyText(tx.sig, exports.getStr(tx))
 
-// todo: verify schema & contents too
+/**
+ * Verifies the given tx.
+ * Returns true if tx is valid. Throws an assert.AssertionError with a relevant message, if the verification fails.
+ * todo: verify schema & contents too (as we do with block.verify)
+ */
 exports.verify = tx => exports.verifySig(tx)
