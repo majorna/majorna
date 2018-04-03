65d12ac4ce2654c6fdc280770377053fb16dfb20
Teoman Soygul <tsoygul@gmail.com>
2018-04-03 22:53:03 +0200

    ui: shop: add sample item

--- src/comps/account/Shop.js
+++ src/comps/account/Shop.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 export default props =>
   <React.Fragment>
-    <div className="mj-box">
-      <span>Wow</span>
+    <div className="mj-box flex-column">
+      <span>Crowner</span>
+      <img width="350" src="http://via.placeholder.com/350x150" alt="loremipsum"/>
     </div>
   </React.Fragment>
