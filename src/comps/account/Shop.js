d09f006a32b6faccff5a5916e1dcae84f081ebb1
Teoman Soygul <tsoygul@gmail.com>
2018-04-05 15:54:07 +0200

    ui: shop: grid layout

--- src/comps/account/Shop.js
+++ src/comps/account/Shop.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
 export default props =>
-  <div className="mj-box">
-    <div className="mj-box flex-column">
+  <div className="mj-box shop-grid">
+    <div className="mj-box flex-column center-all">
       <span>Crowner</span>
-      <img src="http://via.placeholder.com/150x150" alt="loremipsum"/>
+      <img width="150px" src="http://via.placeholder.com/150x150" alt="loremipsum"/>
     </div>
 
-    <div className="mj-box flex-column">
+    <div className="mj-box flex-column center-all">
       <span>Crowner</span>
-      <img src="http://via.placeholder.com/150x150" alt="loremipsum"/>
+      <img width="150px" src="http://via.placeholder.com/150x150" alt="loremipsum"/>
     </div>
 
-    <div className="mj-box flex-column">
+    <div className="mj-box flex-column center-all">
       <span>Crowner</span>
-      <img src="http://via.placeholder.com/150x150" alt="loremipsum"/>
+      <img width="150px" src="http://via.placeholder.com/150x150" alt="loremipsum"/>
     </div>
   </div>
