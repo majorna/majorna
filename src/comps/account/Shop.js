2873139a2158d6bbf6cdb451f1f04e391576d0f7
Teoman Soygul <tsoygul@gmail.com>
2018-04-06 14:17:06 +0200

    ui: shop: can purchase majorna

--- src/comps/account/Shop.js
+++ src/comps/account/Shop.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import mj from '../../res/mj.png'
 
 export default props =>
   <React.Fragment>
@@ -8,6 +9,16 @@ export default props =>
 
     <div className="mj-box shop-grid">
       <div className="mj-box flex-column center-all">
+        <span>Majorna</span>
+        <img src={mj} width="150" height="150" alt="Majorna"/>
+      </div>
+
+      <div className="mj-box flex-column center-all">
+        <span>Crowner</span>
+        <img width="150px" src="http://via.placeholder.com/150x150" alt="loremipsum"/>
+      </div>
+
+      <div className="mj-box flex-column center-all">
         <span>Crowner</span>
         <img width="150px" src="http://via.placeholder.com/150x150" alt="loremipsum"/>
       </div>
@@ -24,11 +35,11 @@ export default props =>
     </div>
 
     <div className="mj-box flex-column">
-      Footer text..
+      Footer text about what shop is..
     </div>
   </React.Fragment>
 
-const details = props =>
-  <div className="mj-box flex-column center-all">
-    <div className="is-size-5 has-text-centered">Purchase - {props.product.name}</div>
-  </div>
+// const details = props =>
+//   <div className="mj-box flex-column center-all">
+//     <div className="is-size-5 has-text-centered">Purchase - {props.product.name}</div>
+//   </div>
