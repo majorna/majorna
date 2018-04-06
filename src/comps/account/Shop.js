81e7993de17efec05eff651cedf12ef6f62167e9
Teoman Soygul <tsoygul@gmail.com>
2018-04-06 12:05:28 +0200

    ui: shop: add details component

--- src/comps/account/Shop.js
+++ src/comps/account/Shop.js
@@ -1,19 +1,34 @@
 import React from 'react'
 
 export default props =>
-  <div className="mj-box shop-grid">
-    <div className="mj-box flex-column center-all">
-      <span>Crowner</span>
-      <img width="150px" src="http://via.placeholder.com/150x150" alt="loremipsum"/>
+  <React.Fragment>
+    <div className="mj-box flex-column">
+      <div className="is-size-5 has-text-centered">Shop</div>
     </div>
 
-    <div className="mj-box flex-column center-all">
-      <span>Crowner</span>
-      <img width="150px" src="http://via.placeholder.com/150x150" alt="loremipsum"/>
+    <div className="mj-box shop-grid">
+      <div className="mj-box flex-column center-all">
+        <span>Crowner</span>
+        <img width="150px" src="http://via.placeholder.com/150x150" alt="loremipsum"/>
+      </div>
+
+      <div className="mj-box flex-column center-all">
+        <span>Crowner</span>
+        <img width="150px" src="http://via.placeholder.com/150x150" alt="loremipsum"/>
+      </div>
+
+      <div className="mj-box flex-column center-all">
+        <span>Crowner</span>
+        <img width="150px" src="http://via.placeholder.com/150x150" alt="loremipsum"/>
+      </div>
     </div>
 
-    <div className="mj-box flex-column center-all">
-      <span>Crowner</span>
-      <img width="150px" src="http://via.placeholder.com/150x150" alt="loremipsum"/>
+    <div className="mj-box flex-column">
+      Footer text..
     </div>
+  </React.Fragment>
+
+const details = props =>
+  <div className="mj-box flex-column center-all">
+    <div className="is-size-5 has-text-centered">Purchase - {props.product.name}</div>
   </div>
