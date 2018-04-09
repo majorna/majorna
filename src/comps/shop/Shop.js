6abb7ce078da8302b57f588dc68a254c6fb0c2e9
Teoman Soygul <tsoygul@gmail.com>
2018-04-09 14:51:39 +0200

    ui: shop: display play store icon on play store items

--- src/comps/shop/Shop.js
+++ src/comps/shop/Shop.js
@@ -41,7 +41,8 @@ export default props =>
 
       <Link className="mj-box flex-column center-all" to="/shop/crowner">
         <span>Crowner</span>
-        <img className="m-t-s" width="150" height="150" src="https://lh3.googleusercontent.com/A8TzrOKsyHUWTEg0-2Qr6aFLvWhCJUq37M4E_BC3qB9JXAjxTnu4GXel3sSa-ysnx_cz=s360" alt="Crowner"/>
+        <img className="m-t-xs" width="150" src="https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png" alt="Get it on Google Play"/>
+        <img className="m-t-xs" width="150" height="150" src="https://lh3.googleusercontent.com/A8TzrOKsyHUWTEg0-2Qr6aFLvWhCJUq37M4E_BC3qB9JXAjxTnu4GXel3sSa-ysnx_cz=s360" alt="Crowner"/>
         <button className="button is-small is-info is-outlined m-t-s">Details</button>
       </Link>
     </div>
