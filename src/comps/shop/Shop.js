95542e6718a7528438555ee1658ac4aaba092acc
Teoman Soygul <tsoygul@gmail.com>
2018-04-09 16:04:56 +0200

    ui: shop: add title description

--- src/comps/shop/Shop.js
+++ src/comps/shop/Shop.js
@@ -4,8 +4,9 @@ import mj from '../../res/mj.png'
 
 export default props =>
   <React.Fragment>
-    <div className="mj-box flex-column">
-      <div className="is-size-5 has-text-centered">Shop</div>
+    <div className="mj-box flex-column center-all">
+      <div className="is-size-5">Shop</div>
+      <div className="m-t-s">Currently all items in the shop are only for preview. You can vote for future products to be made available at the bottom of this page.</div>
     </div>
 
     <div className="mj-box shop-grid">
