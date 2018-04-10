60ec57a3cb3e6c24329345b83f5c231f305dec81
Teoman Soygul <tsoygul@gmail.com>
2018-04-10 13:43:37 +0200

    ui: about: organize page layout

--- src/comps/about/About.js
+++ src/comps/about/About.js
@@ -1,12 +1,15 @@
-// majorna
+// # majorna
 // - link to roadmap
 // - link to tech
 // - intro (what it is & goals)
 // - link to tech page for details)
 
-/// why majorna
+// ## why majorna
+// - ease of use (no download required, browser only, one click signup)
+// - amazingly simple in comparison to other cryptos with key generation etc. all on user, which can all go wrong
+// - optional opt-in for many advanced features (i.e. own private keys etc.)
 
-/// the team
+// ## the team
 
-/// the vision
-/// - long term goals (and link to roadmap complete with time goals)
+// ## the vision
+// - long term goals (and link to roadmap complete with time goals)
