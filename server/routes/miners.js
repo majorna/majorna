ae14e3b87bb25af2d2734c3947afd20a0590fa69
Teoman Soygul <tsoygul@gmail.com>
2018-04-11 21:46:58 +0200

    svr: miner: improve miner location float handling

--- server/routes/miners.js
+++ server/routes/miners.js
@@ -6,10 +6,10 @@ const miners = require('../nodes/miners')
  */
 exports.set = route.post('/miners', async ctx => {
   const txBody = ctx.request.body
-  ctx.assert(Number.isInteger(txBody.lat), 400, '"lat" field is required.')
-  ctx.assert(Number.isInteger(txBody.lon), 400, '"long" field is required.')
+  ctx.assert(Number.isInteger(txBody.lat) || (txBody.lat && parseFloat(txBody.lat)), 400, '"lat" field is required.')
+  ctx.assert(Number.isInteger(txBody.lon) || (txBody.lon && parseFloat(txBody.lon)), 400, '"long" field is required.')
 
-  miners.addMiner(ctx.state.user.uid, txBody.lat, txBody.lon)
+  miners.addMiner(ctx.state.user.uid, parseFloat(txBody.lat), parseFloat(txBody.lon))
 
   ctx.body = {miners: miners.miners}
   ctx.status = 201
