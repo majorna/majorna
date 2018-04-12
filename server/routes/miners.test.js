6b675cfc79b4e820525f381332ce0cb4ac9b1d17
Teoman Soygul <tsoygul@gmail.com>
2018-04-12 15:30:38 +0200

    svr: miners: fix test

--- server/routes/miners.test.js
+++ server/routes/miners.test.js
@@ -2,7 +2,7 @@ const assert = require('assert')
 const testData = require('../config/test').data
 
 suite('route: miners', () => {
-  test.only('post', async () => {
+  test('post', async () => {
     // post location
     const id = testData.users.u1Auth.uid
     const res = await testData.users.u1Request.post('/miners', {id, lat: 0, lon: 123})
