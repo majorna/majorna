5c08fd1efa5639fe346d5a7b1f86db115afc3d94
Teoman Soygul <tsoygul@gmail.com>
2018-04-17 01:31:29 +0200

    ui: home: highlight important stats

--- src/comps/home/Home.js
+++ src/comps/home/Home.js
@@ -22,27 +22,27 @@ export default props =>
     {!props.mjMetaDoc ? <div className="mj-box center-all spinner"/> :
       <div className="mj-box home-stats-grid">
         <div className="flex-column center-all">
-          <strong className="is-size-5">${props.mjMetaDoc.val}</strong>
+          <strong className="is-size-5 has-text-success">${props.mjMetaDoc.val}</strong>
           Current Price
         </div>
         <div className="flex-column center-all">
-          <strong className="is-size-5">${fm(props.mjMetaDoc.marketCap * props.mjMetaDoc.val)}</strong>
+          <strong className="is-size-5 has-text-success">${fm(props.mjMetaDoc.marketCap * props.mjMetaDoc.val)}</strong>
           Market Cap.
         </div>
         <div className="flex-column center-all">
-          <strong className="is-size-5">{fm(props.mjMetaDoc.marketCap / 1000000)}M mj</strong>
-          Current Supply
-        </div>
-        <div className="flex-column center-all">
-          <strong className="is-size-5">{fm(props.mjMetaDoc.maxSupply / 1000000)}M mj</strong>
-          Max Supply
-        </div>
-        <div className="flex-column center-all">
-          <strong className="is-size-5">${fm(props.mjMetaDoc.monthly.txVolume * props.mjMetaDoc.val)}</strong>
+          <strong className="is-size-5 has-text-success">${fm(props.mjMetaDoc.monthly.txVolume * props.mjMetaDoc.val)}</strong>
           Tx Volume (monthly)
         </div>
         <div className="flex-column center-all">
-          <strong className="is-size-5">{props.mjMetaDoc.userCount}</strong>
+          <strong className="is-size-5 has-text-grey">{fm(props.mjMetaDoc.marketCap / 1000000)}M mj</strong>
+          Current Supply
+        </div>
+        <div className="flex-column center-all">
+          <strong className="is-size-5 has-text-grey">{fm(props.mjMetaDoc.maxSupply / 1000000)}M mj</strong>
+          Max Supply
+        </div>
+        <div className="flex-column center-all">
+          <strong className="is-size-5 has-text-grey">{props.mjMetaDoc.userCount}</strong>
           Total Accounts
         </div>
       </div>
