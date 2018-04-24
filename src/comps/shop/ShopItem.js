8e48bacb0ba890e0ebe35031f9010e2dc165f96c
Teoman Soygul <tsoygul@gmail.com>
2018-04-24 14:19:25 +0200

    ui: shop: load widget script conditionally

--- src/comps/shop/ShopItem.js
+++ src/comps/shop/ShopItem.js
@@ -3,15 +3,16 @@ import builtInItems from './BuiltInItems'
 
 export default props => {
   const item = builtInItems.find(i => i.id === props.match.params.id)
+  if (item.id === 'majorna') {
+    // todo: use component did mount
+    window.requestAnimationFrame(() => {
+      const script = document.createElement('script')
+      script.src = 'https://commerce.coinbase.com/v1/checkout.js'
+      document.getElementById('action-buttons').appendChild(script)
+    })
+  }
 
   function handleBuy () {}
-
-  // todo: use component did mount
-  window.requestAnimationFrame(() => {
-    const script = document.createElement('script')
-    script.src = 'https://commerce.coinbase.com/v1/checkout.js'
-    document.getElementById('action-buttons').appendChild(script)
-  })
 
   return (
     <div className="mj-box flex-column center-all box-center w-m">
@@ -33,7 +34,7 @@ export default props => {
       {item.unavailable && <strong className="m-t-m">Status: <span className="has-text-warning">Unavailable</span></strong>}
 
       <div id="action-buttons" className="flex-row m-t-l">
-        <a className="button is-info donate-with-crypto" disabled={item.unavailable} href="https://commerce.coinbase.com/checkout/3e67bb92-c9e8-42c3-a832-48f8bfc67e84" onClick={handleBuy}>Buy</a>
+        <a className="button is-info donate-with-crypto" disabled={item.unavailable} href={item.externalUrl} onClick={handleBuy}>Buy</a>
         <button className="button m-l-m" onClick={props.history.goBack}>Cancel</button>
       </div>
     </div>
