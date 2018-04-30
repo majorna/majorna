8bfef293b9e1380e3f8261b3058c386c6db181f9
Teoman Soygul <tsoygul@gmail.com>
2018-04-30 11:37:35 +0200

    svr: data: cb: add currency note

--- server/data/coinbase.js
+++ server/data/coinbase.js
@@ -5,3 +5,6 @@
  * Coinbase Commerce Integration:
  * - API ref: https://commerce.coinbase.com/docs/api/
  */
+
+// todo: use curl https://api.coinbase.com/v2/exchange-rates?currency=USD
+// instead of individual calls
