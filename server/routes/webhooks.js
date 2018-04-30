1f70e691fc14f7bc567b0282b757ba89e679bd45
Teoman Soygul <tsoygul@gmail.com>
2018-04-30 10:28:13 +0200

    readme: integration keys are required for development

--- server/routes/webhooks.js
+++ server/routes/webhooks.js
@@ -1,22 +1,22 @@
 const route = require('koa-route')
-const config = require('../config/config')
+// const config = require('../config/config')
 
 /**
  * Accept payments through Coinbase Commerce: https://commerce.coinbase.com/docs/api/#webhooks
  */
 exports.coinbaseCommerce = route.post('/webhooks/coinbase-commerce', async ctx => {
   // verify raw payload signature
-  const sharedSecret = config.integrations.coinbaseCommerce.webhookSharedSecret
-  const sig = ctx.headers['X-CC-Webhook-Signature']
-  const payload = ctx.request.rawBody
-
-  // webhook payload is legit so log the details
-  const data = ctx.request.body
-  console.log('Incoming Coinbase Commerce webhook:', data)
-
-  if (data.event.type === 'charge:confirmed') {
-
-  }
+  // const sharedSecret = config.integrations.coinbaseCommerce.webhookSharedSecret
+  // const sig = ctx.headers['X-CC-Webhook-Signature']
+  // const payload = ctx.request.rawBody
+  //
+  // // webhook payload is legit so log the details
+  // const data = ctx.request.body
+  // console.log('Incoming Coinbase Commerce webhook:', data)
+  //
+  // if (data.event.type === 'charge:confirmed') {
+  //
+  // }
 
   // need to return 200 otherwise coinbase retries
 })
