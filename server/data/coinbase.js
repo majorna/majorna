cab514dbe3833a417e475eaf6926db569ea8a603
Teoman Soygul <tsoygul@gmail.com>
2018-05-02 22:35:07 +0200

    ui: shop: fix wording

--- server/data/coinbase.js
+++ server/data/coinbase.js
@@ -19,7 +19,7 @@ exports.createCharge = async userId => {
   const res = await axios.post('https://api.commerce.coinbase.com/charges',
     {
       name: 'Majorna',
-      description: 'Get Majorna using other cryptos.',
+      description: 'Get Majorna using other cryptocurrencies.',
       logo_url: config.app.logoUrl,
       redirect_url: config.app.url,
       pricing_type: 'no_price',
