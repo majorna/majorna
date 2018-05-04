f008a1773cf86dbf17c4fc0645402fd36ac1b8d2
Teoman Soygul <tsoygul@gmail.com>
2018-05-04 09:43:50 +0200

    ui: mine: we are on mainnet!

--- src/comps/account/Mine.js
+++ src/comps/account/Mine.js
@@ -129,12 +129,5 @@ export default class extends Component {
       <div className="flex-row center-h m-t-l">
         <button className="button" onClick={this.handleStop}>Stop</button>
       </div>
-
-      <small className="m-t-l">
-        <i>
-          All the mined blocks are on testnet until July 2018.
-          They may or may not be transferred (in part or fully) to the mainnet, depending on security analysis.
-        </i>
-      </small>
     </div>
 }
