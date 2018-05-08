6632591e6845561acc2b2ee83ac9a81e5666f4c2
Teoman Soygul <tsoygul@gmail.com>
2018-05-08 20:05:57 +0200

    svr: stripe: post data as url encoded form data

--- server/data/stripe.js
+++ server/data/stripe.js
@@ -1,31 +1,41 @@
 const assert = require('assert')
+const querystring = require('querystring')
 const axios = require('axios')
 const config = require('../config/config')
 
+const usdMultiplier = 100
+
+/**
+ * Creates a Stripe charge with given params.
+ * @param token - Token acquired from Stripe checkout or elements.
+ * @param amountUsd - Amount in US Dollars (not cents, whole Dollars without fractions).
+ * @param amountMj - Matching amount in Majorna.
+ * @returns {Promise.<number>} - A promise that resolves to actually charged amount in whole US Dollars.
+ */
 exports.createCharge = async (token, amountUsd, amountMj) => {
   assert(token, '"token" parameter is required')
   assert(amountUsd, '"amountUsd" parameter is required')
   assert(amountMj, '"amountMj" parameter is required')
 
+  amountUsd = amountUsd * usdMultiplier
+
   const res = await axios.post('https://api.stripe.com/v1/charges',
-    {
+    querystring.stringify({
       amount: amountUsd,
       currency: 'usd',
       description: `Purchase of ${amountMj} mj.`,
-      source: token,
-    },
+      source: token
+    }),
     {
       auth: {
-        username: config.integrations.stripe.secretKey,
-        password: ''
+        username: config.integrations.stripe.secretKey
       }
     })
 
-  assert(res.status === 201)
+  assert(res.status === 200)
   assert(res.data.captured)
   assert(res.data.paid)
-  assert(!config.app.isProd && !res.data.livemode)
-  assert(config.app.isProd && res.data.livemode)
+  assert(config.app.isProd ? res.data.livemode : !res.data.livemode)
   assert(!res.data.refunded)
-  return res.data.amount
+  return res.data.amount / usdMultiplier
 }
--- server/data/stripe.test.js
+++ server/data/stripe.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const stripe = require('./stripe')
 
-suite.only('stripe', () => {
+suite('stripe', () => {
   test('createCharge', async () => {
     const amount = await stripe.createCharge('tok_visa', 10, 100)
     assert(amount === 10)
