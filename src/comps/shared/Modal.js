b3f91862202861836664b2aca6f2e8e4c2442230
Teoman Soygul <tsoygul@gmail.com>
2018-05-13 19:42:31 +0200

    ui: shared: modal bg is white

--- src/comps/shared/Modal.js
+++ src/comps/shared/Modal.js
@@ -6,11 +6,11 @@ import React from 'react'
 export default props =>
   <div className={'modal' + (props.children ? ' is-active' : '')}>
     <div className="modal-background"/>
-    <div className="modal-content">
+    <div className="modal-content mj-box flex-column center-h bg-white">
       {props.children}
 
       <div className="flex-row center-h m-t-l">
-        <button className="button" onClick={props.clearNotification}>Close</button>
+        <button className="button is-info" onClick={props.clearNotification}>Close</button>
       </div>
     </div>
     {props.topRightCloseButton && <button className="modal-close is-large"/>}
