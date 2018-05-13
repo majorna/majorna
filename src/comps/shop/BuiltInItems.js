1bed8603292672112d3fcf61b0862fe38708f840
Teoman Soygul <tsoygul@gmail.com>
2018-05-13 20:52:33 +0200

    ui: shop: open support url in new tab

--- src/comps/shop/BuiltInItems.js
+++ src/comps/shop/BuiltInItems.js
@@ -9,9 +9,9 @@ export default [
     id: 'majorna',
     name: 'Majorna',
     description: () => <React.Fragment>
-      Buy Majorna using Bitcoin, Ethereum, or other cryptos.
+      Buy Majorna using Card or Bitcoin, Ethereum, or other cryptos.
       Amount sent will be converted to mj and deposited in your account within 15 minutes.
-      You can report problems about purchases to <a href={'mailto:support@' + config.hosting.domain}>support@{config.hosting.domain}</a>.
+      You can report problems about purchases to <a href={'mailto:support@' + config.hosting.domain} target="_blank" rel="noopener noreferrer">support@{config.hosting.domain}</a>
     </React.Fragment>,
     imageUrl: mj,
     stripeScriptUrl: 'https://js.stripe.com/v3/',
