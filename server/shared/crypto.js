b90929b3702fc1cbf7fe5c210b73d26dcdadae95
Teoman Soygul <tsoygul@gmail.com>
2018-05-25 14:27:54 +0200

    svr: shared: factor out common config

--- server/shared/crypto.js
+++ server/shared/crypto.js
@@ -1,15 +1,16 @@
-const crypto = require('crypto')
-const config = require('../config/config')
+const config = require('./config')
 
-// todo: move these to config
-exports.algo = 'SHA256'
-// todo: can use DER encoding for signature and save ~20bytes: https://stackoverflow.com/a/39651457/628273 (or compressed ec sig for further reduction)
-exports.encoding = 'base64'
+if (config.app.isNode) {
+  const crypto = require('crypto')
 
-exports.hashTextToBuffer = text => crypto.createHash(exports.algo).update(text).digest()
+  exports.hashTextToBuffer = text => crypto.createHash(config.crypto.algo).update(text).digest()
 
-exports.hashText = text => exports.hashTextToBuffer(text).toString(exports.encoding)
+  exports.hashText = text => exports.hashTextToBuffer(text).toString(config.crypto.encoding)
 
-exports.signText = text => crypto.createSign(exports.algo).update(text).sign(config.crypto.privateKey, exports.encoding)
+  exports.signText = text => crypto.createSign(config.crypto.algo).update(text).sign(config.crypto.privateKey, config.crypto.encoding)
 
-exports.verifyText = (sig, text) => crypto.createVerify(exports.algo).update(text).verify(config.crypto.publicKey, sig, exports.encoding)
+  exports.verifyText = (sig, text) => crypto.createVerify(config.crypto.algo).update(text).verify(config.crypto.publicKey, sig, config.crypto.encoding)
+
+  return
+}
+
