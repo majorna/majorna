76e1ad64851732295cdee5a534479ada3e6328ad
Teoman Soygul <tsoygul@gmail.com>
2018-05-25 12:11:34 +0200

    svr: shared: use class methods instead of fields

--- server/shared/tx.js
+++ server/shared/tx.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-// const crypto = require('./crypto')
+const crypto = require('./crypto')
 
 assert({
   sig: '', // signature of the tx, signed by the sender (or majorna on behalf of sender)
@@ -16,7 +16,7 @@ assert({
   amount: 0 // amount being sent
 })
 
-module.exports = class {
+module.exports = class Tx {
   constructor (sig, id, fromId, fromBalance, toId, toBalance, time, amount) {
     this.sig = sig
     this.id = id
@@ -31,31 +31,41 @@ module.exports = class {
     this.time = time
     this.amount = amount
   }
-  //
-  // /**
-  //  * Concatenates the the given tx into a regular string, fit for hashing.
-  //  */
-  // getStr = () => '' + this.id + this.from.id + this.from.balance + this.to.id + this.to.balance + this.time.getTime() + this.amount
-  //
-  // /**
-  //  * Creates a valid tx object out of a given object. Extra fields will not be used.
-  //  */
-  // static getObj = tx => new module.exports(tx.sig, tx.id, tx.from.id, tx.from.balance, tx.to.id, tx.to.balance, tx.time, tx.amount)
-  //
-  // /**
-  //  * Signs the tx with majorna certificate.
-  //  */
-  // sign = () => { this.sig = crypto.signText(this.getStr()) }
-  //
-  // /**
-  //  * Verifies the tx's signature.
-  //  */
-  // verifySig = () => crypto.verifyText(this.sig, this.getStr())
-  //
-  // /**
-  //  * Verifies the tx.
-  //  * Returns true if tx is valid. Throws an assert.AssertionError with a relevant message, if the verification fails.
-  //  * todo: verify schema & contents too (as we do with block.verify)
-  //  */
-  // verify = () => this.verifySig()
+
+  /**
+   * Concatenates the the given tx into a regular string, fit for hashing.
+   */
+  getStr () {
+    return '' + this.id + this.from.id + this.from.balance + this.to.id + this.to.balance + this.time.getTime() + this.amount
+  }
+
+  /**
+   * Creates a valid tx object out of a given object. Extra fields will not be used.
+   */
+  static getObj (tx) {
+    return new Tx(tx.sig, tx.id, tx.from.id, tx.from.balance, tx.to.id, tx.to.balance, tx.time, tx.amount)
+  }
+
+  /**
+   * Signs the tx with majorna certificate.
+   */
+  sign () {
+    this.sig = crypto.signText(this.getStr())
+  }
+
+  /**
+   * Verifies the tx's signature.
+   */
+  verifySig () {
+    return crypto.verifyText(this.sig, this.getStr())
+  }
+
+  /**
+   * Verifies the tx.
+   * Returns true if tx is valid. Throws an assert.AssertionError with a relevant message, if the verification fails.
+   */
+  verify () {
+    // todo: verify schema & contents too (as we do with block.verify)
+    return this.verifySig()
+  }
 }
