bb544e646792a5131b0e8683289e7674e5204d8a
Teoman Soygul <tsoygul@gmail.com>
2018-06-04 23:56:10 +0200

    ui: tx: fix layout

--- src/comps/account/Tx.js
+++ src/comps/account/Tx.js
@@ -8,22 +8,24 @@ export default props => {
   return <div className="mj-box flex-column box-center w-m">
     <div className="is-size-5 has-text-centered">Transaction Details</div>
 
-    <div><strong>Tx ID:</strong> {tx.id}</div>
-
+    <div className="m-t-m"><strong>Tx ID:</strong> {tx.id}</div>
     <div><strong className="m-t-m">Amount:</strong> mj{fm(tx.amount)}</div>
-
     <div><strong>Time:</strong> {tx.time.toLocaleString()}</div>
 
     {tx.from ?
-      <div><strong>From:</strong> {tx.fromName} - {tx.from}</div> :
-      <div><strong>From:</strong> {props.userDoc.name} - {props.user.uid}</div>}
+      <div className="m-t-m"><strong>From:</strong> {tx.fromName} - {tx.from}</div> :
+      <div className="m-t-m"><strong>From:</strong> {props.userDoc.name} - {props.user.uid}</div>}
 
     {tx.to ?
       <div><strong>To:</strong> {tx.toName} - {tx.to}</div> :
       <div><strong>To:</strong> {props.userDoc.name} - {props.user.uid}</div>}
 
-    <div><strong>Search in Blockchain Repo:</strong> <a href={githubLink} target="_blank" rel="noopener noreferrer">{githubLink}</a></div>
+    <div className="m-t-m"><strong>In Block:</strong> <a href={githubLink} target="_blank" rel="noopener noreferrer">{githubLink}</a></div>
 
-    <small>(Transaction data might take up to a day to be indexed by GitHub.)</small>
+    <small className="m-t-m">(Transaction data might take up to a day to be indexed by GitHub)</small>
+
+    <div className="flex-row center-h m-t-m">
+      <button className="button m-t-l" onClick={props.history.goBack}>Back</button>
+    </div>
   </div>
 }
