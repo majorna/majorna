0e1117211c06f47841cbedacb368c71de2af349d
Teoman Soygul <tsoygul@gmail.com>
2018-06-06 20:52:11 +0200

    svr: don't let log export throw error

--- server/index.js
+++ server/index.js
@@ -4,12 +4,16 @@ const firebaseConfig = require('./config/firebase')
 console.log = (...args) => {
   const logLine = util.format.apply(null, args)
   process.stdout.write(logLine + '\n')
-  firebaseConfig.log.info(firebaseConfig.log.entry(logLine)).catch(e => process.stderr.write(e))
+  try {
+    firebaseConfig.log.info(firebaseConfig.log.entry(logLine)).catch(e => process.stderr.write(e))
+  } catch (e) { console.error(e) }
 }
 console.error = (...args) => {
   const logLine = util.format.apply(null, args)
   process.stderr.write(logLine + '\n')
-  firebaseConfig.log.error(firebaseConfig.log.entry(logLine)).catch(e => process.stderr.write(e))
+  try {
+    firebaseConfig.log.error(firebaseConfig.log.entry(logLine)).catch(e => process.stderr.write(e))
+  } catch (e) { console.error(e) }
 }
 
 const server = require('./config/server')
