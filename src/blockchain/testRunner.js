e4a0ca7948ae59a8a2a91686937662b2da78b65c
Teoman Soygul <tsoygul@gmail.com>
2018-06-18 12:08:16 +0200

    ui: blockchain: note when tests start and end

--- src/blockchain/testRunner.js
+++ src/blockchain/testRunner.js
@@ -3,10 +3,12 @@ import Tx from './Tx.e2e'
 const tests = {Tx}
 
 export default () => {
+  console.log('[Running tests]')
   Object.entries(tests).forEach(t => {
     Object.entries(t[1]).forEach(async c => {
-      console.log(`${t[0]}: ${c[0]}`)
+      console.log(`\t${t[0]}: ${c[0]}`)
       await (c[1]())
     })
   })
+  console.log('[Tests complete]')
 }
