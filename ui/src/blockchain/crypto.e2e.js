82f6a8d33953c2b174a6b33963a4fa68738c8c10
Teoman Soygul <tsoygul@gmail.com>
2018-06-21 14:53:32 +0200

    ui: test: half b64 return

--- ui/src/blockchain/crypto.e2e.js
+++ ui/src/blockchain/crypto.e2e.js
@@ -8,11 +8,12 @@ export default {
   'sign and verify': async () => {
     const text = 'lorem ipsum dolor'
     const sig = await signText(text)
+    console.log(sig)
     assert(await verifyText(sig, text))
 
-    const text2 = 'asdf89u -098sd7fsadufih sadfh0isaudf09-2ui3/;sd3/.,mOI_(*YT*(^FTIDTXipf90as.sdafsdas djf-9i1j?KJPOih-9?oiuasdf83348'
-    const sig2 = await signText(text2)
-    assert(await verifyText(sig2, text2))
+    // const text2 = 'asdf89u -098sd7fsadufih sadfh0isaudf09-2ui3/;sd3/.,mOI_(*YT*(^FTIDTXipf90as.sdafsdas djf-9i1j?KJPOih-9?oiuasdf83348'
+    // const sig2 = await signText(text2)
+    // assert(await verifyText(sig2, text2))
 
     // assert(sig2.length === 92 || sig2.length === 96)
   }
--- ui/src/blockchain/crypto.js
+++ ui/src/blockchain/crypto.js
@@ -5,12 +5,13 @@ const textEncoder = new TextEncoder('utf-8')
  * Signs given text, asynchronously.
  * Returned promise resolves to the signature.
  */
-export function signText(text) {
-  return crypto.subtle.sign(
+export async function signText(text) {
+  const sigBuff = await crypto.subtle.sign(
     {name: config.crypto.signAlgo, hash: config.crypto.hashAlgo},
     config.crypto.privateKey,
     textEncoder.encode(text)
   )
+  return sigBuff
 }
 
 /**
@@ -18,10 +19,12 @@ export function signText(text) {
  * Returned promise resolves to a boolean.
  */
 export function verifyText(sig, text) {
+  const asig = btoa(String.fromCharCode(...new Uint8Array(sig)))
+
   return crypto.subtle.verify(
     {name: config.crypto.signAlgo, hash: config.crypto.hashAlgo},
     config.crypto.publicKey,
-    sig instanceof ArrayBuffer ? sig : textEncoder.encode(sig),
+    sig instanceof ArrayBuffer ? sig : textEncoder.encode(atob(asig)),
     text instanceof ArrayBuffer ? text : textEncoder.encode(text)
   )
 }
