cec3b8c49b98d6682969a829f0c48d7bdce20fd3
Teoman Soygul <tsoygul@gmail.com>
2018-06-21 09:25:26 +0200

    ui: crypto: add docs

--- ui/src/blockchain/crypto.js
+++ ui/src/blockchain/crypto.js
@@ -1,9 +1,17 @@
 import config from '../data/config'
 
+/**
+ * Signs given text, asynchronously.
+ * Returned promise resolves to the signature.
+ */
 export function signText(text) {
   return crypto.subtle.sign(config.crypto.algo, config.crypto.privateKey, text)
 }
 
+/**
+ * Verifies given text with signature, asynchronously.
+ * Returned promise resolves to a boolean.
+ */
 export function verifyText(sig, text) {
   return crypto.subtle.verify(config.crypto.algo, config.crypto.publicKey, sig, text)
 }
