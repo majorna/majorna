7c672c1cf2bd5e9c97499eb822f6745faade4a43
Teoman Soygul <tsoygul@gmail.com>
2018-06-25 21:52:43 +0200

    ui: blockchain: add rest of merkle methods

--- ui/src/blockchain/Merkle.js
+++ ui/src/blockchain/Merkle.js
@@ -1,3 +1,16 @@
+async function calcNextLevel(topLevel) {
+  const nodes = []
+  const topLevelCount = topLevel.length
+  for (let x = 0; x < topLevelCount; x += 2) {
+    if (x + 1 <= topLevelCount - 1) { // concatenate and hash the pair, add to the next level array
+      nodes.push(await crypto.hashTextToBuffer(Buffer.concat([topLevel[x], topLevel[x + 1]])))
+    } else { // this is an odd ending node, promote up to the next level by itself
+      nodes.push(topLevel[x])
+    }
+  }
+  return nodes
+}
+
 export default class {
   tree = {
     leaves: [], // flat array of ArrayBuffer of item hashes
@@ -14,27 +27,11 @@ export default class {
   makeTree = async () => {
     this.tree.levels.unshift(this.tree.leaves)
     while (this.tree.levels[0].length > 1) {
-      this.tree.levels.unshift(_calculateNextLevel(doubleHash))
+      this.tree.levels.unshift(await calcNextLevel(this.tree.levels[0]))
     }
-  }
-
-  _calculateNextLevel = () => {
-    const nodes = []
-    const topLevel = tree.levels[0]
-    const topLevelCount = topLevel.length
-    for (let x = 0; x < topLevelCount; x += 2) {
-      if (x + 1 <= topLevelCount - 1) { // concatenate and hash the pair, add to the next level array, doubleHash if requested
-        if (doubleHash) {
-          nodes.push(hashFunction(hashFunction(Buffer.concat([topLevel[x], topLevel[x + 1]]))))
-        } else {
-          nodes.push(hashFunction(Buffer.concat([topLevel[x], topLevel[x + 1]])))
-        }
-      } else { // this is an odd ending node, promote up to the next level by itself
-        nodes.push(topLevel[x])
-      }
-    }
-    return nodes
   }
 
   getMerkleRoot = () => this.tree.levels[0][0]
+
+  // todo: getProof & validateProof and add tests & publish package
 }
