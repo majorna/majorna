b57d698d2bcafe173ade8f62f9333fba580e839b
Teoman Soygul <tsoygul@gmail.com>
2018-06-25 23:49:52 +0200

    ui: blockchain: no need to have fns on top due to hoisting

--- ui/src/blockchain/Merkle.js
+++ ui/src/blockchain/Merkle.js
@@ -1,16 +1,3 @@
-async function calcNextLevel(topLevel) {
-  const nodes = []
-  const topLevelCount = topLevel.length
-  for (let x = 0; x < topLevelCount; x += 2) {
-    if (x + 1 <= topLevelCount - 1) { // concatenate and hash the pair, add to the next level array
-      nodes.push(await crypto.hashTextToBuffer(Buffer.concat([topLevel[x], topLevel[x + 1]])))
-    } else { // this is an odd ending node, promote up to the next level by itself
-      nodes.push(topLevel[x])
-    }
-  }
-  return nodes
-}
-
 export default class {
   tree = {
     leaves: [], // flat array of ArrayBuffer of item hashes
@@ -34,4 +21,17 @@ export default class {
   getMerkleRoot = () => this.tree.levels[0][0]
 
   // todo: getProof & validateProof and add tests & publish package
+}
+
+async function calcNextLevel(topLevel) {
+  const nodes = []
+  const topLevelCount = topLevel.length
+  for (let x = 0; x < topLevelCount; x += 2) {
+    if (x + 1 <= topLevelCount - 1) { // concatenate and hash the pair, add to the next level array
+      nodes.push(await crypto.hashTextToBuffer(Buffer.concat([topLevel[x], topLevel[x + 1]])))
+    } else { // this is an odd ending node, promote up to the next level by itself
+      nodes.push(topLevel[x])
+    }
+  }
+  return nodes
 }
