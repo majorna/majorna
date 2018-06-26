027069cef6f2a382f5c94b014b981026242e7d3f
Teoman Soygul <tsoygul@gmail.com>
2018-06-26 13:43:12 +0200

    ui: blockchain: add utf-16 note

--- ui/src/blockchain/crypto.js
+++ ui/src/blockchain/crypto.js
@@ -10,7 +10,7 @@ export function base64ToArrayBuffer(base64) {
   const len = binary_string.length
   const bytes = new Uint8Array(len)
   for (let i = 0; i < len; i++)        {
-    bytes[i] = binary_string.charCodeAt(i)
+    bytes[i] = binary_string.charCodeAt(i) // utf-16
   }
   return bytes
 }
