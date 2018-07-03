99a192b9460f68569bab6f4f0591dd87ed1f5c6c
Teoman Soygul <tsoygul@gmail.com>
2018-07-03 15:17:18 +0200

    ui: blockchain: add hash test

--- ui/src/blockchain/crypto.e2e.js
+++ ui/src/blockchain/crypto.e2e.js
@@ -1,6 +1,9 @@
 import assert from './assert'
 import config from '../data/config'
-import { convertBufferToHexStr, convertHexStrToBuffer, signStrToHexStr, verifyStrWithHexStrSig } from './crypto'
+import {
+  convertBufferToHexStr, convertHexStrToBuffer, hashStrToHexStr, signStrToHexStr,
+  verifyStrWithHexStrSig
+} from './crypto'
 
 const buffer = new Uint8Array([0, 1, 2, 42, 100, 101, 102, 255]).buffer
 
@@ -13,6 +16,14 @@ export default {
 
     const bufferParsedUint8Array = new Uint8Array(bufferParsed)
     assert(new Uint8Array(buffer).every((v, i) => v === bufferParsedUint8Array[i]))
+  },
+
+  'hash': async () => {
+    const str = 'piasdf098usdahofiusd-9fihbosduafhoisadufh-oihweew34dsaffdsaf'
+    const sha256Hex = 'C810A71CBCBE7458DBF371590AA76395033347558CA8583F540B381E612FE2A7'
+
+    const computedSha256Hex = await hashStrToHexStr(str)
+    assert.equal(sha256Hex, computedSha256Hex.toUpperCase())
   },
 
   'sign and verify': async () => {
