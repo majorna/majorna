7d1cc1d01774c5ad3d985fe6c3967f1a1c4c3057
Teoman Soygul <tsoygul@gmail.com>
2018-07-04 20:22:51 +0200

    ui: bc: tx: add test conditions to verify the verification

--- ui/src/blockchain/Tx.e2e.js
+++ ui/src/blockchain/Tx.e2e.js
@@ -40,10 +40,46 @@ export default {
     invalidIdTx.id = 123
     await assert.throws(() => invalidIdTx.verify(), 'id must be')
 
+    // invalid from
+    const invalidFromTx = getSampleTx()
+    await invalidFromTx.sign()
+    invalidFromTx.from = {}
+    await assert.throws(() => invalidFromTx.verify(), 'from id')
+    invalidFromTx.from = {id: 'abc123'}
+    await assert.throws(() => invalidFromTx.verify(), 'from balance')
+    invalidFromTx.from = {id: 'abc123', balance: 0}
+    await assert.throws(() => invalidFromTx.verify(), 'from balance')
+
+    // invalid to
+    const invalidToTx = getSampleTx()
+    await invalidToTx.sign()
+    invalidToTx.to = {}
+    await assert.throws(() => invalidToTx.verify(), 'to id')
+    invalidToTx.to = {id: 'abc123'}
+    await assert.throws(() => invalidToTx.verify(), 'to balance')
+    invalidToTx.to = {id: 'abc123', balance: -1}
+    await assert.throws(() => invalidToTx.verify(), 'to balance')
+
+    // same to and from
+    const sameToAndFromTx = getSampleTx()
+    await sameToAndFromTx.sign()
+    sameToAndFromTx.from = sameToAndFromTx.to
+    await assert.throws(() => sameToAndFromTx.verify(), 'to and from')
+
     // invalid time
-    // const invalidTimeTx = getSampleTx()
-    // invalidTimeTx.time = '01 Jan 2010 00:00:00 UTC'
-    // await invalidTimeTx.sign()
-    // await assert.throws(() => invalidTimeTx.verify(), 'time object')
+    const invalidTimeTx = getSampleTx()
+    await invalidTimeTx.sign()
+    await invalidTimeTx.sign()
+    invalidTimeTx.time = '01 Jan 2010 00:00:00 UTC'
+    await assert.throws(() => invalidTimeTx.verify(), 'time object')
+
+    // invalid amount
+    const invalidAmountTx = getSampleTx()
+    await invalidAmountTx.sign()
+    await invalidAmountTx.sign()
+    invalidAmountTx.amount = '123'
+    await assert.throws(() => invalidAmountTx.verify(), 'amount must be')
+    invalidAmountTx.amount = 0
+    await assert.throws(() => invalidAmountTx.verify(), 'amount must be')
   }
 }
