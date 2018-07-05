77bf82d24e864794249e43df996c15c4f4397ad1
Teoman Soygul <tsoygul@gmail.com>
2018-07-05 20:32:06 +0200

    ui: remove link to legacy post

--- ui/src/comps/shared/TestnetWidget.js
+++ ui/src/comps/shared/TestnetWidget.js
@@ -2,8 +2,5 @@ import React from 'react'
 
 export default props =>
   <small className="flex-row center-all has-text-weight-bold m-t-m" style={{color: 'orange'}}>
-    {/*<i className="fas fa-flask m-r-s"/>TESTNET*/}
-    <a href="https://www.reddit.com/r/majorna/comments/8r8o3y/mainnet_is_shutting_down/" target="_blank" rel="noopener noreferrer">
-      <i className="fas fa-flask m-r-s"/><span style={{textDecoration: 'underline'}}>TESTNET</span>
-    </a>
+    <i className="fas fa-flask m-r-s"/>TESTNET
   </small>
