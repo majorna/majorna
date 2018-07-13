52ca60f6aeef31d37b0c01ec3b82526965251235
Teoman Soygul <tsoygul@gmail.com>
2018-07-13 18:52:16 +0200

    ui: bc: separate peer and mash tests

--- ui/src/blockchain/node.e2e.js
+++ ui/src/blockchain/node.e2e.js
@@ -1,33 +1,43 @@
 import Peer from 'simple-peer'
 
 export default {
-  'peer mesh test': async () => {
-    // These are peer1's connections to peer2 and peer3
-    const peer2 = new Peer({ initiator: true })
-    const peer3 = new Peer({ initiator: true })
+  'webrtc single peer': () => {
+    const peer1 = new Peer({initiator: true, trickle: false})
+    const peer2 = new Peer({trickle: false})
 
-    peer2.on('signal', function (data) {
-      // send this signaling data to peer2 somehow
-    })
+    peer1.on('signal', data => peer2.signal(data))
+    peer2.on('signal', data => peer1.signal(data))
 
-    peer2.on('connect', function () {
-      peer2.send('hi peer2, this is peer1')
-    })
+    peer1.on('connect', () => peer1.send('hey peer2, how is it going?'))
 
-    peer2.on('data', function (data) {
-      console.log('got a message from peer2: ' + data)
-    })
+    peer2.on('data', data => console.log('got a message from peer1: ' + data))
+  },
 
-    peer3.on('signal', function (data) {
-      // send this signaling data to peer3 somehow
-    })
-
-    peer3.on('connect', function () {
-      peer3.send('hi peer3, this is peer1')
-    })
-
-    peer3.on('data', function (data) {
-      console.log('got a message from peer3: ' + data)
-    })
+  'webrtc mesh network': () => {
+    // // create mesh channels
+    // const peer1toPeer2 = new Peer({initiator: true, trickle: false})
+    // const peer1toPeer3 = new Peer({initiator: true, trickle: false})
+    //
+    // const peer2toPeer1 = new Peer()
+    // const peer2toPeer3 = new Peer({initiator: true, trickle: false})
+    //
+    // const peer3toPeer1 = new Peer()
+    // const peer3toPeer2 = new Peer()
+    //
+    // // handle init signals
+    // peer1toPeer2.on('signal', data => peer2toPeer1.signal(data))
+    // peer1toPeer3.on('signal', data => peer3toPeer1.signal(data))
+    //
+    // peer2toPeer1.on('signal', data => peer1toPeer2.signal(data))
+    // peer2toPeer3.on('signal', data => peer3toPeer2.signal(data))
+    //
+    // peer3toPeer1.on('signal', data => peer1toPeer3.signal(data))
+    // peer3toPeer2.on('signal', data => peer2toPeer3.signal(data))
+    //
+    // // handle incoming data
+    // peer1toPeer2.on('data', data => console.log('peer 1 got message from peer 2: ', data))
+    //
+    // // handle connect events
+    // peer2toPeer1.on('connect', () => peer2toPeer1.send('hello peer 1'))
   }
 }
