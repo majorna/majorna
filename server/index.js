5dbdf2feb911fcdcb0b9a753eb413b0d8267b102
Teoman Soygul <tsoygul@gmail.com>
2018-07-18 00:45:31 +0200

    ui: bc: prevent cyclic logging

--- server/index.js
+++ server/index.js
@@ -6,14 +6,18 @@ console.log = (...args) => {
   process.stdout.write(logLine + '\n')
   try {
     logLine && firebaseConfig.log.info(firebaseConfig.log.entry(logLine)).catch(e => process.stderr.write(e))
-  } catch (e) { console.error(e) }
+  } catch (e) {
+    process.stderr.write(e + '\n')
+  }
 }
 console.error = (...args) => {
   const logLine = util.format.apply(null, args)
   process.stderr.write(logLine + '\n')
   try {
     logLine && firebaseConfig.log.error(firebaseConfig.log.entry(logLine)).catch(e => process.stderr.write(e))
-  } catch (e) { console.error(e) }
+  } catch (e) {
+    process.stderr.write(e + '\n')
+  }
 }
 
 const server = require('./config/server')
