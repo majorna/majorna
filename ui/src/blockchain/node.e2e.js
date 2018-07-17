92adf207e8d6921ff9df2bd712cdd16a24219796
Teoman Soygul <tsoygul@gmail.com>
2018-07-17 17:30:54 +0200

    ui: bc: compact config list

--- ui/src/blockchain/node.e2e.js
+++ ui/src/blockchain/node.e2e.js
@@ -4,17 +4,9 @@ export default {
   'webrtc single peer': () => new Promise((resolve, reject) => {
     const config = {
       iceServers: [
-        {
-          urls: 'stun:stun.l.google.com:19302'
-        },
-        {
-          urls: 'stun:global.stun.twilio.com:3478?transport=udp'
-        },
-        {
-          urls: 'turn:numb.viagenie.ca',
-          username: 'peer2peer@pokemail.net',
-          credential: 'peer2peer'
-        }
+        {urls: 'stun:stun.l.google.com:19302'},
+        {urls: 'stun:global.stun.twilio.com:3478?transport=udp'},
+        {urls: 'turn:numb.viagenie.ca', username: 'peer2peer@pokemail.net', credential: 'peer2peer'}
       ]
     }
     const peer1 = new Peer({initiator: true, trickle: false, config})
