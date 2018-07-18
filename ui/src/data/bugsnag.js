13b47b53aa9efc88b4bfac8329220f99a45975e0
Teoman Soygul <tsoygul@gmail.com>
2018-07-18 20:44:56 +0200

    ui: data: enable bugs nag session capture for now

--- ui/src/data/bugsnag.js
+++ ui/src/data/bugsnag.js
@@ -4,10 +4,11 @@ import config from './config'
 const bugsnagClient = !config.app.isTest ? bugsnag({
   apiKey: '58d2de70068cdb078c2a00c88f1e8b13',
   autoBreadcrumbs: false,
-  autoCaptureSessions: false,
   autoNotify: false,
-  collectUserIp: false,
-  user: {id: '1234'} // todo: set user ID
+
+  // autoCaptureSessions: false,
+  // collectUserIp: false,
+  // user: {id: '1234'} // todo: set user ID and uncomment above 3 lines
 }) : {}
 
 // bugsnagClient.notify(new Error('Test error'))
