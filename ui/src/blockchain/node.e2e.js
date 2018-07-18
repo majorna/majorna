8c41204ae07a1c8500c8ba1b6bad5627ad3186ce
Teoman Soygul <tsoygul@gmail.com>
2018-07-18 14:15:43 +0200

    ui: bc: enable mesh test

--- ui/src/blockchain/node.e2e.js
+++ ui/src/blockchain/node.e2e.js
@@ -1,9 +1,9 @@
 import Peer from 'simple-peer'
 
+Peer.config.iceServers.push({urls: 'turn:numb.viagenie.ca', username: 'peer2peer@pokemail.net', credential: 'peer2peer'})
+
 export default {
   'webrtc single peer': () => new Promise((resolve, reject) => {
-    Peer.config.iceServers.push({urls: 'turn:numb.viagenie.ca', username: 'peer2peer@pokemail.net', credential: 'peer2peer'})
-
     const peer1 = new Peer({initiator: true, trickle: false})
     const peer2 = new Peer({trickle: false})
 
@@ -13,40 +13,51 @@ export default {
     peer1.on('signal', data => peer2.signal(data))
     peer2.on('signal', data => peer1.signal(data))
 
+    peer1.on('connect', () => peer1.send('hello peer 2'))
+
     peer2.on('data', data => {
       peer1.destroy()
       peer2.destroy()
-      data.toString() === 'peer1msg' ? resolve() : reject(`got unexpected message from peer 1: ${data}`)
+      data.toString() === 'hello peer 2' ? resolve() : reject(`got unexpected message from peer 1: ${data}`)
     })
-
-    peer1.on('connect', () => peer1.send('peer1msg'))
   }),
 
-  'webrtc mesh network': () => {
-    // // create mesh channels
-    // const peer1toPeer2 = new Peer({initiator: true, trickle: false})
-    // const peer1toPeer3 = new Peer({initiator: true, trickle: false})
-    //
-    // const peer2toPeer1 = new Peer()
-    // const peer2toPeer3 = new Peer({initiator: true, trickle: false})
-    //
-    // const peer3toPeer1 = new Peer()
-    // const peer3toPeer2 = new Peer()
-    //
-    // // handle init signals
-    // peer1toPeer2.on('signal', data => peer2toPeer1.signal(data))
-    // peer1toPeer3.on('signal', data => peer3toPeer1.signal(data))
-    //
-    // peer2toPeer1.on('signal', data => peer1toPeer2.signal(data))
-    // peer2toPeer3.on('signal', data => peer3toPeer2.signal(data))
-    //
-    // peer3toPeer1.on('signal', data => peer1toPeer3.signal(data))
-    // peer3toPeer2.on('signal', data => peer2toPeer3.signal(data))
-    //
-    // // handle incoming data
-    // peer1toPeer2.on('data', data => console.log('peer 1 got message from peer 2: ', data))
-    //
-    // // handle connect events
-    // peer2toPeer1.on('connect', () => peer2toPeer1.send('hello peer 1'))
-  }
+  'webrtc mesh network': () => new Promise((resolve, reject) => {
+    // create mesh channels
+    const peer1toPeer2 = new Peer({initiator: true, trickle: false})
+    const peer1toPeer3 = new Peer({initiator: true, trickle: false})
+
+    const peer2toPeer1 = new Peer({trickle: false})
+    const peer2toPeer3 = new Peer({initiator: true, trickle: false})
+
+    const peer3toPeer1 = new Peer({trickle: false})
+    const peer3toPeer2 = new Peer({trickle: false})
+
+    // handle errors
+    peer1toPeer2.on('error', e => reject(e))
+    peer1toPeer3.on('error', e => reject(e))
+
+    peer2toPeer1.on('error', e => reject(e))
+    peer2toPeer3.on('error', e => reject(e))
+
+    peer3toPeer1.on('error', e => reject(e))
+    peer3toPeer2.on('error', e => reject(e))
+
+    // handle init signals
+    peer1toPeer2.on('signal', data => peer2toPeer1.signal(data))
+    peer1toPeer3.on('signal', data => peer3toPeer1.signal(data))
+
+    peer2toPeer1.on('signal', data => peer1toPeer2.signal(data))
+    peer2toPeer3.on('signal', data => peer3toPeer2.signal(data))
+
+    peer3toPeer1.on('signal', data => peer1toPeer3.signal(data))
+    peer3toPeer2.on('signal', data => peer2toPeer3.signal(data))
+
+    // handle connect events
+    peer1toPeer2.on('connect', () => peer1toPeer2.send('hello peer 2'))
+
+    // handle incoming data
+    // todo: use the entire mesh and close the channels in the end
+    peer2toPeer1.on('data', data => data.toString() === 'hello peer 2' ? resolve() : reject(`got unexpected message from peer 1: ${data}`))
+  })
 }
