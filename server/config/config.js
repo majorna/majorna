3589c36d33b64562cc6777396ed8e86240c131c9
Teoman Soygul <tsoygul@gmail.com>
2018-08-02 15:55:27 +0200

    svr: config: shift rewards towards lower latency

--- server/config/config.js
+++ server/config/config.js
@@ -103,9 +103,9 @@ MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAE2yLEGhHZMiClLt4rHm6Kajo2qsRRQMUW
 const blockchain = {
   blockInterval: (app.isProd ? 10 : 3) * 60 * 1000, // ms
 
-  initialMinBlockDifficulty: app.isProd ? 8 : 1,
+  initialMinBlockDifficulty: app.isProd ? 7 : 1,
   blockDifficultyIncrementStep: 1,
-  difficultyRewardMultiplier: app.isProd ? 0.15 : 1 // (reward multiplier < 1 breaks tests due to math.round)
+  difficultyRewardMultiplier: app.isProd ? 0.12 : 1 // (reward multiplier < 1 breaks tests due to math.round)
 }
 
 // 3rd party integrations
