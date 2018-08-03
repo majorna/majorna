b8b674c8957560a161287dabc6a78f55977518a2
Teoman Soygul <tsoygul@gmail.com>
2018-08-03 14:29:00 +0200

    ui: bc: handle peer events

--- ui/src/blockchain/PeerNetwork.js
+++ ui/src/blockchain/PeerNetwork.js
@@ -1,4 +1,18 @@
+import { InitiatingPeer } from './Peer'
+
 export default class PeerNetwork {
+  peers = []
+
+  initPeer = () => {
+    const peer = new InitiatingPeer()
+    peer.on('error', e => console.error(e))
+    peer.on('close', () => {})
+    peer.on('signal', data => server.initPeer(data))
+    peer.on('connect', () => console.log('peer successfully initialized:', peer))
+    peer.on('data', data => console.log(data))
+    this.peers.push(peer)
+  }
+
   receiveTxs = () => {
     // no duplicates
     // no balance below 0
