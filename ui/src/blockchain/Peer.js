309d698723f6cc5e117c0fc868c5af1db056baca
Teoman Soygul <tsoygul@gmail.com>
2018-08-08 15:01:43 +0200

    ui: bc: turn server note

--- ui/src/blockchain/Peer.js
+++ ui/src/blockchain/Peer.js
@@ -1,5 +1,6 @@
 import Peer from 'simple-peer'
 
+// keep hardcoded STUN servers and additional TURN server in case all other P2P connection option fails
 Peer.config.iceServers.push({urls: 'turn:numb.viagenie.ca', username: 'peer2peer@pokemail.net', credential: 'peer2peer'})
 
 export class InitiatingPeer extends Peer {
