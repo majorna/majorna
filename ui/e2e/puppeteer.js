02aaaf414f14a0366b5de62a10b8599ac603a423
Teoman Soygul <tsoygul@gmail.com>
2018-08-23 21:07:09 +0200

    add e2e testing with puppeteer

---
+++ ui/e2e/puppeteer.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer')
+
+(async () => {
+  // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#running-puppeteer-on-travis-ci
+  const browser = process.env.CI ? await puppeteer.launch({args: ['--no-sandbox']}) : await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('http://localhost:3002')
+
+  // todo: check logs for [Test END] -or- [Test FAIL]: page.on('console', msg => ...)
+
+  await browser.close()
+})()
