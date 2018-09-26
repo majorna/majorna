b763efde4b2c4aa6f3daea5c7a65dd885b9f48ee
Teoman Soygul <tsoygul@gmail.com>
2018-09-26 11:45:40 +0200

    svr: routes: fix test routes

--- server/routes/peers.test.js
+++ server/routes/peers.test.js
@@ -5,17 +5,17 @@ suite('route: miners', () => {
   test('post', async () => {
     // post location
     const id = testData.users.u1Auth.uid
-    const res = await testData.users.u1Request.post('/miners', { id, lat: 0, lon: 123 })
+    const res = await testData.users.u1Request.post('/peers/miners', { id, lat: 0, lon: 123 })
     assert(res.status === 201)
     assert(res.data.miners.find(m => m.lon === 123))
 
     // update location
-    const res2 = await testData.users.u1Request.post('/miners', { id, lat: 1.234, lon: 0 })
+    const res2 = await testData.users.u1Request.post('/peers/miners', { id, lat: 1.234, lon: 0 })
     assert(res2.status === 201)
     assert(res2.data.miners.find(m => m.lat === 1.234))
 
     // invalid request
-    const res3 = await testData.users.u1Request.post('/miners', { id, lon: 3.4 })
+    const res3 = await testData.users.u1Request.post('/peers/miners', { id, lon: 3.4 })
     assert(res3.status === 400)
     assert(!res3.data.miners)
   })
