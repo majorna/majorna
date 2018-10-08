215b863fb556218616581b34b5d84b1084ec05cc
Teoman Soygul <tsoygul@gmail.com>
2018-10-08 17:22:33 +0200

    svr: bc: document onInitPeerList

--- server/data/peers.js
+++ server/data/peers.js
@@ -2,6 +2,19 @@ const utils = require('./utils')
 
 const miners = [] // todo: purge offline miners before interacting with this array (heroku restart does the business once a day, for the moment)
 
+const minerSchema = {
+  lat: 1.1234,
+  lon: 1.1234,
+  lastOnline: new Date(),
+  onInitPeerList: [
+    {
+      resolve: () => {},
+      reject: () => {},
+      time: new Date()
+    }
+  ]
+}
+
 /**
  * Adds a miner to the miner map. If the miner exists on the map, the coordinates are updated.
  * Returns list of miners with lat & lon properties.
@@ -14,7 +27,7 @@ exports.addMiner = (id, lat, lon) => {
     miner.lon = lon
     miner.lastOnline = lastOnline
   } else {
-    miners.push({ id, lat, lon, lastOnline: lastOnline })
+    miners.push({ id, lat, lon, lastOnline: lastOnline, onInitPeerList: [] })
   }
 
   return miners.map(m => ({ lat: m.lat, lon: m.lon }))
