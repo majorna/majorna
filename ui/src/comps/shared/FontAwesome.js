e659a6535bdce4d31e3b0fabbb867399c993f114
Teoman Soygul <tsoygul@gmail.com>
2018-10-10 17:37:23 +0200

    ui: update icon packages to latest

--- ui/src/comps/shared/FontAwesome.js
+++ ui/src/comps/shared/FontAwesome.js
@@ -1,43 +1,57 @@
-import fontawesome from '@fortawesome/fontawesome'
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
 
-import farPlusSquare from '@fortawesome/fontawesome-free-regular/faPlusSquare'
-import farMinusSquare from '@fortawesome/fontawesome-free-regular/faMinusSquare'
-import farQuestionCircle from '@fortawesome/fontawesome-free-regular/faQuestionCircle'
+import {
+  faPlusSquare,
+  faMinusSquare,
+  faQuestionCircle
+} from '@fortawesome/free-regular-svg-icons'
 
-import fasCreditCard from '@fortawesome/fontawesome-free-solid/faCreditCard'
-import fasGlobe from '@fortawesome/fontawesome-free-solid/faGlobe'
-import fasPaperPlane from '@fortawesome/fontawesome-free-solid/faPaperPlane'
-import fasQrcode from '@fortawesome/fontawesome-free-solid/faQrcode'
-import fasTh from '@fortawesome/fontawesome-free-solid/faTh'
-import fasCheck from '@fortawesome/fontawesome-free-solid/faCheck'
-import fasDollarSign from '@fortawesome/fontawesome-free-solid/faDollarSign'
-import fasEuroSign from '@fortawesome/fontawesome-free-solid/faEuroSign'
-import fasShoppingCart from '@fortawesome/fontawesome-free-solid/faShoppingCart'
-import fasSignInAlt from '@fortawesome/fontawesome-free-solid/faSignInAlt'
-import fasFlask from '@fortawesome/fontawesome-free-solid/faFlask'
+import {
+  faCreditCard,
+  faGlobe,
+  faPaperPlane,
+  faQrcode,
+  faTh,
+  faCheck,
+  faDollarSign,
+  faEuroSign,
+  faShoppingCart,
+  faSignInAlt,
+  faFlask
+} from '@fortawesome/free-solid-svg-icons'
 
-import fabGithub from '@fortawesome/fontawesome-free-brands/faGithub'
-import fabReddit from '@fortawesome/fontawesome-free-brands/faReddit'
-import fabBitcoin from '@fortawesome/fontawesome-free-brands/faBitcoin'
-import fabEthereum from '@fortawesome/fontawesome-free-brands/faEthereum'
+import {
+  faGithub,
+  faReddit,
+  faBitcoin,
+  faEthereum
+} from '@fortawesome/free-brands-svg-icons'
 
-fontawesome.library.add(farPlusSquare)
-fontawesome.library.add(farMinusSquare)
-fontawesome.library.add(farQuestionCircle)
+library.add(
+  faPlusSquare,
+  faMinusSquare,
+  faQuestionCircle
+)
 
-fontawesome.library.add(fasCreditCard)
-fontawesome.library.add(fasGlobe)
-fontawesome.library.add(fasPaperPlane)
-fontawesome.library.add(fasQrcode)
-fontawesome.library.add(fasTh)
-fontawesome.library.add(fasCheck)
-fontawesome.library.add(fasDollarSign)
-fontawesome.library.add(fasEuroSign)
-fontawesome.library.add(fasShoppingCart)
-fontawesome.library.add(fasSignInAlt)
-fontawesome.library.add(fasFlask)
+library.add(
+  faCreditCard,
+  faGlobe,
+  faPaperPlane,
+  faQrcode,
+  faTh,
+  faCheck,
+  faDollarSign,
+  faEuroSign,
+  faShoppingCart,
+  faSignInAlt,
+  faFlask
+)
 
-fontawesome.library.add(fabGithub)
-fontawesome.library.add(fabReddit)
-fontawesome.library.add(fabBitcoin)
-fontawesome.library.add(fabEthereum)
+library.add(
+  faGithub,
+  faReddit,
+  faBitcoin,
+  faEthereum
+)
+
+dom.watch() // kicks off the process of finding <i> tags and replacing with <svg>
