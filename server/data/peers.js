ba8fcf3700bc1fcb3dca2751ef32ff1be714f649
Teoman Soygul <tsoygul@gmail.com>
2018-10-11 15:34:53 +0200

    svr: data: await for peer signal data

--- server/data/peers.js
+++ server/data/peers.js
@@ -3,18 +3,19 @@ const db = require('./db')
 
 const miners = [] // todo: purge offline miners before interacting with this array (heroku restart does the business once a day, for the moment)
 
-// const minerSchema = {
-//   lat: 1.1234,
-//   lon: 1.1234,
-//   lastOnline: new Date(),
-//   onInitPeerList: [
-//     {
-//       resolve: () => {},
-//       reject: () => {},
-//       time: new Date()
-//     }
-//   ]
-// }
+miners.schema = {
+  lat: 1.1234,
+  lon: 1.1234,
+  lastOnline: new Date(),
+  onInitPeerList: [
+    {
+      id: 'abcd1234',
+      resolve: () => {},
+      reject: () => {},
+      time: new Date()
+    }
+  ]
+}
 
 /**
  * Adds a miner to the miner map. If the miner exists on the map, the coordinates are updated.
@@ -34,12 +35,18 @@ exports.addMiner = (id, lat, lon) => {
   return miners.map(m => ({ lat: m.lat, lon: m.lon }))
 }
 
+/**
+ *
+ * @param id - ID of the user that is calling this function.
+ * @param signalData - WebRTC signal data produced by the user calling this function.
+ * @returns {Promise<*>}
+ */
 exports.initPeer = async (id, signalData) => {
   // get a random miner from list that is not us
   const filteredMiners = miners.filter(m => m.id !== id)
   const miner = filteredMiners[utils.getRandomInt(0, filteredMiners.length - 1)]
   await db.addNotification({ type: 'webRTCInit', signalData })
-  return new Promise((resolve, reject) => {
+  await new Promise((resolve, reject) => {
     miner.onInitPeerList.push({ resolve, reject, time: new Date() })
   })
 }
