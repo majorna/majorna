aa29dbfdc0789f7656adeb95abc0acfa0486743b
Teoman Soygul <teo@soygul.com>
2019-04-06 23:27:01 +0200

    e2e: close chrome after test success/fail

--- ui/e2e/puppeteer.js
+++ ui/e2e/puppeteer.js
@@ -2,11 +2,12 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#running-puppeteer-on-travis-ci
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch(/*{headless: false}*/)
   const page = await browser.newPage()
   await page.goto('http://localhost:3002')
 
-  // todo: check logs for [Test END] -or- [Test FAIL]: page.on('console', msg => ...)
+  // todo: need to login to initiate tests
 
-  await browser.close()
+  // page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  page.on('console', async msg => msg.text() === '[Test END]' || msg.text() === '[Test FAIL]' && await browser.close())
 })()
