49ccf36f83fa76ea4d77351cbcec138e3da606ca
Teoman Soygul <teo@soygul.com>
2019-04-06 23:16:21 +0200

    e2e: comma after require is required!

--- ui/e2e/puppeteer.js
+++ ui/e2e/puppeteer.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
 
 (async () => {
   // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#running-puppeteer-on-travis-ci
