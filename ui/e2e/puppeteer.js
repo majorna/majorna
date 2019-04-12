e698fea5620ac3cf943d6db89700ade3c2dedb94
Teoman Soygul <teo@soygul.com>
2019-04-12 14:32:59 +0200

    e2e: log UI console in debug mode

--- ui/e2e/puppeteer.js
+++ ui/e2e/puppeteer.js
@@ -4,12 +4,13 @@
  */
 const puppeteer = require('puppeteer')
 
-const testDebug = true
+const debugTestEnv = true
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 ;(async () => {
-  const browser = await puppeteer.launch(testDebug ? {headless: false} : undefined)
+  const browser = await puppeteer.launch(debugTestEnv ? {headless: false} : undefined)
   const page = await browser.newPage()
+  debugTestEnv && page.on('console', msg => console.log('UI console.log:', msg.text()))
 
   // wait until ui server starts
   for (let i = 0; i < 10; i++) {
@@ -24,7 +25,6 @@ const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
   // todo: need to login to initiate tests
 
-  // page.on('console', msg => console.log('PAGE LOG:', msg.text()));
   page.on('console', async msg => msg.text() === '[Test END]' || msg.text() === '[Test FAIL]' && await browser.close())
 
   console.log('E2E tests done!')
