84d710919df83e42b4bbb39b97da40120af0eaba
Teoman Soygul <teo@soygul.com>
2021-10-28 16:16:20 +0200

    comps: update sample with some more hooks

--- ui/src/comps/Sample.js
+++ ui/src/comps/Sample.js
@@ -15,8 +15,20 @@ export default props =>
 export default props => {
     const [page, setPage] = useState('Home')
 
+    // similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+      // update the document title using the browser API
+      document.title = `You are still on page ${page}`;
+
+      // cleanup stage to be executed on unmount
+      return () => {setPage('Home')}
+    });
+
     return <React.Fragment>
-      Current page is: {page}
+      Current page is: ${page}
+        <button onClick={() => setPage('Search')}>
+          Click to Search
+        </button>
     </React.Fragment>
 }
 
