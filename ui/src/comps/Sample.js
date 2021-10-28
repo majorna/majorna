0774374092fe9a0bffc01ce69270c71f4bc68a13
Teoman Soygul <teo@soygul.com>
2021-10-28 15:30:19 +0200

    comps: update sample with react hooks

--- ui/src/comps/Sample.js
+++ ui/src/comps/Sample.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 
+// one liner
+export default props => <div>Wow</div>
+
+// multi one liner
 export default props =>
   <React.Fragment>
     <div className="mj-box">
@@ -7,21 +11,27 @@ export default props =>
     </div>
   </React.Fragment>
 
-const OneLiner = props => <div>Wow</div>
+// using react hooks (no classes)
+export default props => {
+    const [page, setPage] = useState('Home')
 
-// export default props => <div>Wow</div>
-//
-// export default class extends Component {
-//   state = {
-//     abc: ''
-//   }
-//
-//   componentDidMount = () => {/*start network requests here*/ /*rendering of dom is complete in-mem so we can manipulate dom now*/}
-//
-//   handleCancel = () => this.props.history.goBack()
-//
-//   render = () =>
-//     <React.Fragment>
-//       abc
-//     </React.Fragment>
-// }
+    return <React.Fragment>
+      Current page is: {page}
+    </React.Fragment>
+}
+
+// old-school class based approach
+export default class extends Component {
+  state = {
+    abc: ''
+  }
+
+  componentDidMount = () => {/*start network requests here*/ /*rendering of dom is complete in-mem so we can manipulate dom now*/}
+
+  handleCancel = () => this.props.history.goBack()
+
+  render = () =>
+    <React.Fragment>
+      abc
+    </React.Fragment>
+}
